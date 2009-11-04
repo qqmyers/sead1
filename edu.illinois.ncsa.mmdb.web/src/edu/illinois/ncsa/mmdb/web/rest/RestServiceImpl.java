8d2e7c17bf61357c6395842baeca872358ef1547
Joe Futrelle <futrelle@ncsa.illinois.edu>
2009-11-04 19:25:32 +0000

    first cut at basic REST service

---
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/rest/RestServiceImpl.java
@@ -0,0 +1,244 @@
+package edu.illinois.ncsa.mmdb.web.rest;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.tupeloproject.kernel.BlobRemover;
+import org.tupeloproject.kernel.Context;
+import org.tupeloproject.kernel.ContextFacade;
+import org.tupeloproject.kernel.OperatorException;
+import org.tupeloproject.kernel.SubjectRemover;
+import org.tupeloproject.kernel.Thing;
+import org.tupeloproject.kernel.ThingSession;
+import org.tupeloproject.kernel.impl.MemoryContext;
+import org.tupeloproject.rdf.Resource;
+import org.tupeloproject.rdf.Triple;
+import org.tupeloproject.rdf.terms.Cet;
+import org.tupeloproject.rdf.terms.Rdf;
+
+/**
+ * SimpleRestService
+ */
+public class RestServiceImpl implements RestService, ContextFacade {
+    Context context = new MemoryContext();
+
+    // FIXME replace these with agreed-upon predicates from relevant vocabularies
+    static final Resource IMAGE_TYPE = Cet.cet("mmdb/Image");
+    static final Resource COLLECTION_TYPE = Cet.cet("mmdb/Collection");
+    static final Resource HAS_MEMBER = Cet.cet("mmdb/hasMember"); // maybe dcterms:hasPart?
+
+    public Context getContext() { return context; }
+    public void setContext(Context c) { context = c; }
+
+    /**
+     * Create an image
+     *
+     * @param imageData a stream containing image data
+     * @return a new URI identifying the image
+     */
+    public String createImage(InputStream imageData) throws RestServiceException {
+        String uri = Resource.uriRef().getString();
+        createImage(uri,imageData);
+        return uri;
+    }
+
+    /**
+     * Create an image
+     *
+     * @param imageUri  the URI to give it
+     * @param imageData a stream containing image data
+     */
+    public void createImage(String imageUri, InputStream imageData) throws RestServiceException {
+        updateImage(imageUri, imageData);
+    }
+
+    /**
+     * Retrieve an image. The caller <b>must</b> close the returned stream.
+     *
+     * @param imageUri the URI of the image to retrieve
+     * @return a stream on the image data
+     */
+    public InputStream retrieveImage(String imageUri) throws RestServiceException {
+        try {
+            return getContext().read(Resource.uriRef(imageUri));
+        } catch(OperatorException e) {
+            throw new RestServiceException("could not retrieve image "+imageUri,e);
+        }
+    }
+
+    /**
+     * Update an image
+     *
+     * @param imageUri  the image URI
+     * @param imageData a stream on the data to replace it with
+     */
+    public void updateImage(String imageUri, InputStream imageData) throws RestServiceException {
+        try {
+            Resource subject = Resource.uriRef(imageUri);
+            getContext().write(subject, imageData);
+            getContext().addTriples(Triple.create(subject, Rdf.TYPE, IMAGE_TYPE));
+        } catch(OperatorException e) {
+            throw new RestServiceException("failed to write image "+imageUri,e);
+        }
+    }
+
+    /**
+     * Delete an image
+     *
+     * @param imageUri the image URI
+     */
+    public void deleteImage(String imageUri) throws RestServiceException {
+        Resource subject = Resource.uriRef(imageUri);
+        try {
+            ThingSession s = new ThingSession(getContext());
+            s.delete(subject);
+            s.save();
+            BlobRemover br = new BlobRemover(Resource.uriRef(imageUri));
+            getContext().perform(br);
+        } catch(OperatorException e) {
+            throw new RestServiceException("deletion failed for "+imageUri,e);
+        }
+    }
+
+    /**
+     * Create a collection
+     *
+     * @param members the members of the collection (should be URI's)
+     * @return a new URI identifying the collection
+     */
+    public String createCollection(Iterable<String> members) throws RestServiceException {
+        String subject = Resource.uriRef().getString();
+        updateCollection(subject, members);
+        return subject;
+    }
+
+    // op: 1=replace, 2=add, 3=remove
+    void mutateCollection(String collectionUri, Iterable<String> members, int op) throws RestServiceException {
+        Set<Resource> memberUris = new HashSet<Resource>();
+        for(String member : members) {
+            memberUris.add(Resource.uriRef(member));
+        }
+        try {
+            ThingSession ts = new ThingSession(getContext());
+            Thing collection = ts.fetchThing(Resource.uriRef(collectionUri));
+            collection.addType(COLLECTION_TYPE);
+            switch(op) {
+                case 1: // replace
+                    collection.setValues(HAS_MEMBER, memberUris);
+                    break;
+                case 2: // add
+                    collection.addValues(HAS_MEMBER, memberUris);
+                    break;
+                case 3: // remove
+                    collection.removeValues(HAS_MEMBER, memberUris);
+                    break;
+            }
+            ts.save();
+        } catch(OperatorException e) {
+            throw new RestServiceException("collection update failed for "+collectionUri);
+        }
+    }
+
+    /**
+     * Update a collection
+     *
+     * @param collectionUri the collection URI
+     * @param members       the new set of members (should be URI's)
+     */
+    public void updateCollection(String collectionUri, Iterable<String> members) throws RestServiceException {
+        mutateCollection(collectionUri, members, 1);
+    }
+
+    /**
+     * Retrieve a collection
+     *
+     * @param collectionUri the collection URI
+     * @return the members (a list of URI's)
+     */
+    public List<String> retrieveCollection(String collectionUri) throws RestServiceException {
+        List<String> result = new LinkedList<String>();
+        try {
+            for(Triple t : getContext().match(Resource.uriRef(collectionUri), HAS_MEMBER, null)) {
+                result.add(t.getObject().getString());
+            }
+        } catch(OperatorException e) {
+            throw new RestServiceException("could not fetch collection "+collectionUri);
+        }
+        return result;
+    }
+
+    /**
+     * Delete a collection
+     *
+     * @param collectionUri the collection URI
+     */
+    public void deleteCollection(String collectionUri) throws RestServiceException {
+        try {
+            SubjectRemover sr = new SubjectRemover();
+            sr.setSubject(Resource.uriRef(collectionUri));
+            getContext().perform(sr);
+        } catch(OperatorException e) {
+            throw new RestServiceException("could not remove collection "+collectionUri);
+        }
+    }
+
+    /**
+     * Add to a collection
+     *
+     * @param collectionUri the collection URI
+     * @param members       the members to add (should be URI's)
+     */
+    public void addToCollection(String collectionUri, Iterable<String> members) throws RestServiceException {
+        mutateCollection(collectionUri, members, 2);
+    }
+
+    /**
+     * Remove from a collection
+     *
+     * @param collectionUri the collection URI
+     * @param members       the members to remove (should be URI's)
+     */
+    public void removeFromCollection(String collectionUri, Iterable<String> members) throws RestServiceException {
+        mutateCollection(collectionUri, members, 3);
+    }
+
+    /**
+     * Add tag(s) to a resource
+     *
+     * @param resourceUri the resource URI
+     * @param tags        tags to add (should be tag-like strings (e.g., no spaces))
+     */
+    public void addTags(String resourceUri, Iterable<String> tags) {
+    }
+
+    /**
+     * Remove tag(s) from a resource
+     *
+     * @param resourceUri the resource URI
+     * @param tags        the tags to remove (should be tag-like strings (e.g., no spaces))
+     */
+    public void removeTags(String resourceUri, Iterable<String> tags) {
+    }
+
+    /**
+     * Rate the resource
+     *
+     * @param resourceUri the resource URI
+     * @param rating      1-5
+     */
+    public void setRating(String resourceUri, int rating) {
+    }
+
+    /**
+     * Retrieve the user's rating for the resource
+     *
+     * @param resourceUri the resource URI
+     * @return the rating (1-5)
+     */
+    public int getRating(String resourceUri) {
+        return 0;
+    }
+}
---
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/rest/RestServlet.java
@@ -0,0 +1,188 @@
+package edu.illinois.ncsa.mmdb.web.rest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.xpath.XPathExpressionException;
+import org.w3c.dom.Document;
+
+import org.tupeloproject.util.CopyFile;
+import org.tupeloproject.util.Xml;
+
+import org.xml.sax.SAXException;
+
+/**
+ * RestServlet
+ */
+public class RestServlet extends HttpServlet {
+    static RestService restService; // TODO manage this lifecycle better
+
+    public void init() throws ServletException {
+        super.init();
+        restService = new RestServiceImpl();
+        // TODO make that service aware of the Context
+    }
+
+    /**
+     * the canonical URL of a resource is
+     * {servlet path}{infix}{uri}
+     * for instance if the servlet path is http://mymmdb.org/api
+     * and the infix is /image/
+     * and the URI is urn:foo, the canonical URL is
+     * http://mymmdb.org/api/image/urn:foo
+     *
+     * @param uri the RDF subject of the resource
+     * @param infix the infix to use
+     * @param request the request (so we can extract the server prefix)
+     * @return the canonical URL of the resource
+     */
+    String canonicalizeUri(String uri, String infix, HttpServletRequest request) throws ServletException {
+        try {
+            URL requestUrl = new URL(request.getRequestURL().toString());
+            String prefix = requestUrl.getProtocol()+"://"+requestUrl.getHost();
+            if(requestUrl.getPort() != -1) {
+                prefix = prefix + ":" + requestUrl.getPort();
+            }
+            String canonical = prefix + request.getContextPath() + request.getServletPath() + infix + uri;
+            System.out.println("canonical URI for "+uri+" with infix "+infix+" = "+canonical);//FIXME debug
+            return canonical;
+        } catch(MalformedURLException x) {
+            throw new ServletException("unexpected error: servlet URL is not a URL");
+        }
+    }
+
+    boolean hasPrefix(String uri, String infix, HttpServletRequest request) throws ServletException {
+        return uri.startsWith(canonicalizeUri("",infix,request));
+    }
+
+    boolean hasPrefix(String infix, HttpServletRequest request) throws ServletException {
+        return hasPrefix(request.getRequestURL().toString(),infix,request);
+    }
+
+    String getSuffix(String uri, String infix, HttpServletRequest request) throws ServletException {
+        return uri.substring(canonicalizeUri("",infix,request).length() + 1);
+    }
+    String getSuffix(String infix, HttpServletRequest request) throws ServletException {
+        return getSuffix(request.getRequestURL().toString(), infix, request);
+    }
+
+    void dumpCrap(HttpServletRequest request) {
+        System.out.println("requestURI = "+request.getRequestURI());
+        System.out.println("servletPath = "+request.getServletPath());
+        System.out.println("contextPath = "+request.getContextPath());
+        System.out.println("requestURL = "+request.getRequestURL());
+        System.out.println("localAddr = "+request.getLocalAddr());
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        dumpCrap(request); // FIXME debug
+        if(hasPrefix("/image/",request)) {
+            try {
+                CopyFile.copy(restService.retrieveImage(getSuffix("/image/",request)), response.getOutputStream());
+            } catch(RestServiceException e) {
+                throw new ServletException("failed to retrieve "+request.getRequestURI());
+            }
+        } else if(hasPrefix("/collection/",request)) {
+            try {
+                // TODO currently assumes that everything in a collection is an image
+                // TODO if that assumption is not correct, will need a way to track canonical URL's per-resource
+                // TODO if the collection is huge, this will bloat memory, may need different API to stage
+                List<String> canonicalMembers = new LinkedList<String>();
+                for(String member : restService.retrieveCollection(getSuffix("/collection/",request))) {
+                    canonicalMembers.add(canonicalizeUri(member, "/image/", request));
+                }
+                response.getWriter().write(formatList(canonicalMembers));
+            } catch(RestServiceException e) {
+                throw new ServletException("failed to retrieve "+request.getRequestURI());
+            }
+        } else {
+            throw new ServletException("unrecognized API call "+request.getRequestURI());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        dumpCrap(request); // FIXME debug
+        if(hasPrefix("/image/",request)) {
+            try {
+                restService.updateImage(getSuffix("/image/",request),request.getInputStream());
+            } catch(RestServiceException e) {
+                throw new ServletException("failed to write "+request.getRequestURI());
+            }
+        } else if(hasPrefix("/image",request)) {
+            String uri = null;
+            try {
+                uri = restService.createImage(request.getInputStream());
+            } catch(RestServiceException e) {
+                throw new ServletException("failed to create image",e);
+            }
+            response.getWriter().write(uri+"\n");
+        } else if(hasPrefix("/collection",request)) {
+            // TODO currently assumes that everything in a collection is an image
+            // TODO if that assumption is not correct, will need a way to track canonical URL's per-resource
+            List<String> members = new LinkedList<String>();
+            try {
+                for(String member : parseList(request.getInputStream())) {
+                    if(hasPrefix(member,"/image/",request)) {
+                        members.add(getSuffix(member,"/image/",request));
+                    } else {
+                        members.add(member);
+                    }
+                }
+            } catch(XPathExpressionException e) {
+                throw new ServletException("could not parse collection parameter",e);
+            } catch(SAXException e) {
+                throw new ServletException("could not parse collection parameter",e);
+            }
+            try {
+                if(hasPrefix("/collection/add/",request)) {
+                    restService.addToCollection(getSuffix("/collection/add/",request),members);
+                } else if(hasPrefix("/collection/remove/",request)) {
+                    restService.removeFromCollection(getSuffix("/collection/add/",request),members);
+                } else if(hasPrefix("/collection/",request)) {
+                    restService.updateCollection(getSuffix("/collection/",request),members);
+                } else { // mint a new URI
+                    String uri = restService.createCollection(members);
+                    response.getWriter().write(uri+"\n");
+                }
+            } catch(RestServiceException e) {
+                throw new ServletException("could not modify collection",e);
+            }
+        }
+    }
+
+    // deal with lists
+    String formatList(Iterable<String> members) {
+        StringWriter sw = new StringWriter();
+        sw.append("<ul>");
+        for(String member : members) {
+            sw.append("<li>").append(Xml.escape(member)).append("</li>");
+        }
+        sw.append("</ul>");
+        return sw.toString();
+    }
+
+    List<String> parseList(Document list) throws XPathExpressionException, IOException, SAXException {
+        return Xml.getNodeValues(Xml.evaluateXPath("/ul/li/text()",list));
+    }
+
+    List<String> parseList(InputStream list) throws XPathExpressionException, IOException, SAXException {
+        return parseList(Xml.parse(list));
+    }
+
+    List<String> parseList(Reader list) throws XPathExpressionException, IOException, SAXException {
+        return parseList(Xml.parse(list));
+    }
+
+
+}
