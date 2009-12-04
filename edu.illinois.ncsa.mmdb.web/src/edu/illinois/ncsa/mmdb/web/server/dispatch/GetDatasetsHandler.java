6b6b5b388721b141be4c405cd157ceaede9d492b
Luigi Marini <lmarini@illinois.edu>
2009-12-04 20:37:55 +0000

    Logging and throw ActionException when something is wrong initiating tupelo context.

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/GetDatasetsHandler.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/GetDatasetsHandler.java
@@ -5,6 +5,12 @@ package edu.illinois.ncsa.mmdb.web.server.dispatch;
 
 import java.util.HashSet;
 
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.ActionException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.tupeloproject.kernel.BeanSession;
 
 import edu.illinois.ncsa.mmdb.web.client.dispatch.GetDatasets;
@@ -12,37 +18,43 @@ import edu.illinois.ncsa.mmdb.web.client.dispatch.GetDatasetsResult;
 import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
 import edu.uiuc.ncsa.cet.bean.DatasetBean;
 import edu.uiuc.ncsa.cet.bean.tupelo.DatasetBeanUtil;
-import net.customware.gwt.dispatch.server.ActionHandler;
-import net.customware.gwt.dispatch.server.ExecutionContext;
-import net.customware.gwt.dispatch.shared.ActionException;
 
 /**
  * Handler to retrieve datasets from repository.
  * 
  * @author Luigi Marini
- *
+ * 
  */
-public class GetDatasetsHandler implements ActionHandler<GetDatasets, GetDatasetsResult> {
+public class GetDatasetsHandler implements
+		ActionHandler<GetDatasets, GetDatasetsResult> {
 
 	/** Tupelo bean session **/
-	private static final BeanSession beanSession = TupeloStore.getInstance().getBeanSession();
+	private static final BeanSession beanSession = TupeloStore.getInstance()
+			.getBeanSession();
 
 	/** Datasets DAO **/
 	private static DatasetBeanUtil dbu = new DatasetBeanUtil(beanSession);
-	
+
+	/** Commons logging **/
+	private static Log log = LogFactory.getLog(GetDatasetsHandler.class);
+
 	@Override
 	public GetDatasetsResult execute(GetDatasets arg0, ExecutionContext arg1)
 			throws ActionException {
-		
+
 		HashSet<DatasetBean> datasets = new HashSet<DatasetBean>();
-		
+
 		try {
 			datasets = new HashSet<DatasetBean>(dbu.getAll());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new ActionException(
+					"Error retrieving datasets from Tupelo repository", e);
 		}
-		
+
+		// FIXME here only for debug purposes to help figure out why
+		// some times the table listing datasets doesn't show anything
+		log.debug("Retrieved " + datasets.size() + " datasets from tupelo.");
+
 		return new GetDatasetsResult(datasets);
 	}
 
@@ -55,7 +67,7 @@ public class GetDatasetsHandler implements ActionHandler<GetDatasets, GetDataset
 	public void rollback(GetDatasets arg0, GetDatasetsResult arg1,
 			ExecutionContext arg2) throws ActionException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
