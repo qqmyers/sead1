88f296170f71606abbad89156cb7baeedc7436a9
Luigi Marini <lmarini@illinois.edu>
2009-12-04 20:41:38 +0000

    Initial view for showing a list of datasets in a youtube like view, with one column, attributes layed out so that they are easier to read. Work in progress.

---
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/DatasetTableOneColumnView.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.mmdb.web.client;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.Widget;
+
+import edu.illinois.ncsa.mmdb.web.client.DatasetTablePresenter.Display;
+
+/**
+ * List datasets in repository using a youtube-like list. A one column
+ * table that makes it easier to read attributes of each element.
+ * 
+ * @author Luigi Marini
+ */
+public class DatasetTableOneColumnView extends FlexTable implements Display {
+
+	private final static DateTimeFormat DATE_TIME_FORMAT = DateTimeFormat.getShortDateTimeFormat();
+	
+	private ArrayList<Hyperlink> datasetLinks = new ArrayList<Hyperlink>();
+	
+	public DatasetTableOneColumnView() {
+		super();
+		this.setWidget(0, 0, new Label("Name"));
+		this.setWidget(0, 1, new Label("Type"));
+		this.setWidget(0, 2, new Label("Date"));
+		this.setWidget(0, 3, new Label("*"));
+		addStyleName("datasetTable");
+		getRowFormatter().addStyleName(0, "topRow");
+	}
+
+	@Override
+	public void addRow(String id, String name, String type, Date date) {
+		GWT.log("Adding dataset " + name, null);
+		int row = this.getRowCount();
+		Hyperlink hyperlink = new Hyperlink(name, "dataset?id=" + id);
+		datasetLinks.add(hyperlink);
+		this.setWidget(row, 0, hyperlink);
+		
+		this.setWidget(row, 1, new Label(type));
+		this.setWidget(row, 2, new Label(DATE_TIME_FORMAT.format(date)));
+		
+		// menu
+		MenuBar iconBar = new MenuBar();
+		MenuBar menuBar = new MenuBar(true);
+		iconBar.addItem("Actions", menuBar);
+		menuBar.addItem("Add tag", new Command() {
+			
+			@Override
+			public void execute() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		this.setWidget(row, 3, iconBar);
+		
+		for (int col=0; col<4; col++) {
+			getCellFormatter().addStyleName(row, col, "cell");
+		}
+		
+		if (row % 2 == 0) {
+			getRowFormatter().addStyleName(row, "evenRow");
+		} else {
+			getRowFormatter().addStyleName(row, "oddRow");
+		}
+	}
+
+	@Override
+	public Widget asWidget() {
+		return this;
+	}
+
+}
