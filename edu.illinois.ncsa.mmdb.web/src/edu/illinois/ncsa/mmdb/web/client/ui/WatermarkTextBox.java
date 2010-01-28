6c05483cd846412fc5cbfa6641261048587763d8
Luigi Marini <lmarini@illinois.edu>
2010-01-28 17:54:55 +0000

    Somehow forgot to specify visibility of constructors.

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/WatermarkTextBox.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/WatermarkTextBox.java
@@ -11,8 +11,11 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.TextBox;
 
 /**
+ * A text box that has default text set for when it's empty. The special text to
+ * show when it's empty is the watermark.
+ * 
  * @author Luigi Marini
- *
+ * 
  */
 public class WatermarkTextBox extends TextBox implements FocusHandler,
 		BlurHandler {
@@ -20,24 +23,42 @@ public class WatermarkTextBox extends TextBox implements FocusHandler,
 	private String watermark;
 	private HandlerRegistration blurHandler;
 	private HandlerRegistration focusHandler;
-	
+
+	/**
+	 * No watermark set.
+	 */
 	public WatermarkTextBox() {
 		super();
 		addStyleName("textBoxWatermark");
 	}
-	
-	WatermarkTextBox(String text) {
+
+	/**
+	 * Set content.
+	 * 
+	 * @param text text in box
+	 */
+	public WatermarkTextBox(String text) {
 		this();
 		setText(text);
 	}
-	
-	WatermarkTextBox(String text, String watermark) {
+
+	/**
+	 * Set both content and watermark.
+	 * 
+	 * @param text text in box
+	 * @param watermark watermark for when text is removed
+	 */
+	public WatermarkTextBox(String text, String watermark) {
 		this(text);
 		setWatermark(watermark);
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.google.gwt.event.dom.client.FocusHandler#onFocus(com.google.gwt.event.dom.client.FocusEvent)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.google.gwt.event.dom.client.FocusHandler#onFocus(com.google.gwt.event
+	 * .dom.client.FocusEvent)
 	 */
 	@Override
 	public void onFocus(FocusEvent event) {
@@ -47,14 +68,21 @@ public class WatermarkTextBox extends TextBox implements FocusHandler,
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.google.gwt.event.dom.client.BlurHandler#onBlur(com.google.gwt.event.dom.client.BlurEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.google.gwt.event.dom.client.BlurHandler#onBlur(com.google.gwt.event
+	 * .dom.client.BlurEvent)
 	 */
 	@Override
 	public void onBlur(BlurEvent event) {
 		showWatermark();
 	}
-	
+
+	/**
+	 * Show the watermark.
+	 */
 	private void showWatermark() {
 		if (getText().length() == 0 || getText().equalsIgnoreCase(watermark)) {
 			setText(watermark);
@@ -62,9 +90,14 @@ public class WatermarkTextBox extends TextBox implements FocusHandler,
 		}
 	}
 
+	/**
+	 * Set the value of the watermark.
+	 * 
+	 * @param watermark text to use for the watermark
+	 */
 	public void setWatermark(String watermark) {
 		this.watermark = watermark;
-		
+
 		if (watermark != null && !watermark.equals("")) {
 			blurHandler = addBlurHandler(this);
 			focusHandler = addFocusHandler(this);
@@ -75,6 +108,11 @@ public class WatermarkTextBox extends TextBox implements FocusHandler,
 		}
 	}
 
+	/**
+	 * Get the watermark.
+	 * 
+	 * @return watermark text
+	 */
 	public String getWatermark() {
 		return watermark;
 	}
