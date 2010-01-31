044c505b66fe084483a9a6fe54a7f1323e78ce9a
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-01-31 03:12:17 +0000

    removed spurious accidental import of org.mortbay.log.Log

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/GetPreviewsHandler.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/GetPreviewsHandler.java
@@ -6,7 +6,8 @@ import net.customware.gwt.dispatch.server.ActionHandler;
 import net.customware.gwt.dispatch.server.ExecutionContext;
 import net.customware.gwt.dispatch.shared.ActionException;
 
-import org.mortbay.log.Log;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.tupeloproject.kernel.ThingSession;
 import org.tupeloproject.rdf.Resource;
 import org.tupeloproject.rdf.terms.Cet;
@@ -18,7 +19,8 @@ import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
 import edu.uiuc.ncsa.cet.bean.tupelo.PreviewImageBeanUtil;
 
 public class GetPreviewsHandler implements ActionHandler<GetPreviews, GetPreviewsResult> {
-
+	Log log = LogFactory.getLog(GetPreviewsHandler.class);
+	
 	@Override
 	public GetPreviewsResult execute(GetPreviews getPreviews, ExecutionContext arg1)
 			throws ActionException {
@@ -39,7 +41,7 @@ public class GetPreviewsHandler implements ActionHandler<GetPreviews, GetPreview
 					ThingSession ts = TupeloStore.getInstance().getBeanSession().getThingSession();
 					Date endTime =  ts.getDate(Resource.uriRef(datasetUri), Cet.cet("metadata/extractor/endTime"));
 					if(endTime != null) {
-						Log.info("END TIME = "+endTime);
+						log.debug("telling client to stop asking for previews for "+datasetUri);
 						// there won't be previews, so stop asking!
 						result.setStopAsking(true);
 					}
