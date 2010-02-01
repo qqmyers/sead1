33952adf3f06ccc33dcb7230fb9f445050818f84
Luigi Marini <lmarini@illinois.edu>
2010-02-01 20:38:54 +0000

    MMDB-418 Set the date of when the collection was created.

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/AddCollectionHandler.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/AddCollectionHandler.java
@@ -3,6 +3,8 @@
  */
 package edu.illinois.ncsa.mmdb.web.server.dispatch;
 
+import java.util.Date;
+
 import net.customware.gwt.dispatch.server.ActionHandler;
 import net.customware.gwt.dispatch.server.ExecutionContext;
 import net.customware.gwt.dispatch.shared.ActionException;
@@ -14,38 +16,39 @@ import org.tupeloproject.kernel.BeanSession;
 import edu.illinois.ncsa.mmdb.web.client.dispatch.AddCollection;
 import edu.illinois.ncsa.mmdb.web.client.ui.AddCollectionResult;
 import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
+import edu.uiuc.ncsa.cet.bean.CollectionBean;
 import edu.uiuc.ncsa.cet.bean.tupelo.CollectionBeanUtil;
 
 /**
- * Add a user to a group.
+ * Create a new collection.
  * 
  * @author Luigi Marini
  *
  */
 public class AddCollectionHandler implements ActionHandler<AddCollection, AddCollectionResult> {
 
-	/** Tupelo bean session **/
-	private static final BeanSession beanSession = TupeloStore.getInstance()
-			.getBeanSession();
-
 	/** Commons logging **/
-	private static Log log = LogFactory.getLog(GetCollectionsHandler.class);
-
-	/** Datasets DAO **/
-	private static CollectionBeanUtil cbu = new CollectionBeanUtil(beanSession);
+	private static Log log = LogFactory.getLog(AddCollectionHandler.class);
 	
 	@Override
 	public AddCollectionResult execute(AddCollection arg0, ExecutionContext arg1)
 			throws ActionException {
+		
+		BeanSession beanSession = TupeloStore.getInstance().getBeanSession();
+		
+		CollectionBeanUtil cbu = new CollectionBeanUtil(beanSession);
+		
 		try {
 			log.debug("Adding collection " + arg0.getCollection().getTitle());
 			
 			// FIXME why doesn't update work and we have to use registerAndSave?
 //			CollectionBean collection = cbu.update(arg0.getCollection());
+			
+			CollectionBean collection = arg0.getCollection();
+			collection.setCreationDate(new Date());
 			beanSession.registerAndSave(arg0.getCollection());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error("Error creating new collection", e);
 		}
 		return new AddCollectionResult();
 	}
