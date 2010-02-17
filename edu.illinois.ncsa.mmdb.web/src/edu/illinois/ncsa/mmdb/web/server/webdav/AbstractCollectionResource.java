7d271b029d1182bafbdd7ddfd8f5258e367f977e
Rob Kooper <kooper@illinois.edu>
2010-02-17 04:32:11 +0000

    MMDB-328 contextcreate now takes parameters

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/webdav/AbstractCollectionResource.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/webdav/AbstractCollectionResource.java
@@ -108,7 +108,7 @@ public abstract class AbstractCollectionResource extends AbstractResource implem
         if ( !path.endsWith( "/" ) ) { //$NON-NLS-1$
             path = path + "/"; //$NON-NLS-1$
         }
-
+        //<table><tr><th><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr><tr><th colspan="5"><hr></th></tr>
         XmlWriter w = new XmlWriter( out );
         w.open( "html" ); //$NON-NLS-1$
         w.open( "body" ); //$NON-NLS-1$
