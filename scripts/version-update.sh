96e0003fae0c06724814b9621f222209632631a1
Rob Kooper <kooper@illinois.edu>
2010-03-12 19:32:06 +0000

    Setting version to 0.4.100 and created scripts folder.

---
+++ scripts/version-update.sh
@@ -0,0 +1,31 @@
+FROM="0.0.3"
+TO="0.4.100"
+FOLDERS="../*"
+
+for f in $FOLDERS; do
+  if [ ! -d "$f" ]; then
+    continue
+  fi
+
+  FILE=$f/META-INF/MANIFEST.MF
+  if [ -e "$FILE" ]; then
+    sed -i -e "s/Bundle-Version: ${FROM}.qualifier/Bundle-Version: ${TO}.qualifier/g" "$FILE"
+    if [ "`grep 'Bundle-Vendor:' \"$FILE\"`" = "" ]; then
+      echo "Bundle-Vendor: NCSA" >> "$FILE"
+    fi
+  fi
+
+  FILE=$f/feature.xml
+  if [ -e "$FILE" ]; then
+    sed -i -e "s/version=\"${FROM}.qualifier\"/version=\"${TO}.qualifier\"/g" "$FILE"
+  fi
+
+  for g in "$f"/*.product; do
+    if [ -e "$g" ]; then
+      sed -i -e "s/version=\"${FROM}.qualifier\"/version=\"${TO}.qualifier\"/g" "$g"
+    fi
+  done
+
+done
+
+sed -i -e "s/v${FROM}#/v${TO}#/g" ../edu.illinois.ncsa.mmdb.web/build.xml
