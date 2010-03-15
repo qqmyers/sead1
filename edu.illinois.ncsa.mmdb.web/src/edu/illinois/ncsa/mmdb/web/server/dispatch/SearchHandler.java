2e3531d061137e2369b0f68b223bfb761d4256fd
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-03-15 18:38:17 +0000

    logs execution time.

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/SearchHandler.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/SearchHandler.java
@@ -6,6 +6,10 @@ package edu.illinois.ncsa.mmdb.web.server.dispatch;
 import net.customware.gwt.dispatch.server.ActionHandler;
 import net.customware.gwt.dispatch.server.ExecutionContext;
 import net.customware.gwt.dispatch.shared.ActionException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import edu.illinois.ncsa.cet.search.Hit;
 import edu.illinois.ncsa.cet.search.SearchableTextIndex;
 import edu.illinois.ncsa.mmdb.web.client.dispatch.Search;
@@ -19,16 +23,20 @@ import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
  *
  */
 public class SearchHandler implements ActionHandler<Search, SearchResult> {
-
+	Log log = LogFactory.getLog(SearchHandler.class);
+	
 	@Override
 	public SearchResult execute(Search arg0, ExecutionContext arg1)
 			throws ActionException {
 		SearchResult searchResult = new SearchResult();
 		SearchableTextIndex<String> search = TupeloStore.getInstance().getSearch();
+		long then = System.currentTimeMillis();
 		Iterable<Hit> result = search.search(arg0.getQuery());
 		for (Hit hit : result) {
 				searchResult.addHit(hit.getId());
 		}
+		long elapsed = System.currentTimeMillis() - then;
+		log.debug("Search for '"+arg0.getQuery()+"' took "+elapsed+"ms");
 		return searchResult;
 	}
 
