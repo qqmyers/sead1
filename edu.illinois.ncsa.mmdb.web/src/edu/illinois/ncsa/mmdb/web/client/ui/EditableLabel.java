7e40841077f53fa58a30d2ac74b4191872c328d1
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-05-05 19:52:56 +0000

    resolves MMDB-605

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/EditableLabel.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/EditableLabel.java
@@ -40,6 +40,9 @@ package edu.illinois.ncsa.mmdb.web.client.ui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseOverEvent;
@@ -48,6 +51,8 @@ import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -55,106 +60,118 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 
 public class EditableLabel extends Composite implements HasValueChangeHandlers<String> {
-	HorizontalPanel panel;
-	Label label;
-	boolean isEditable = true;
-	String editableStyleName;
-	
-	public EditableLabel(String text) {
-		super();
-		panel = new HorizontalPanel();
-		label = new Label(text);
-		label.setTitle("Click to edit");
-		panel.add(label);
-		label.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				if(isEditable) {
-					label.removeStyleName("editHighlight");
-					displayEditControls();
-				}
-			}
-		});
-		label.addMouseOverHandler(new MouseOverHandler() {
-			public void onMouseOver(MouseOverEvent event) {
-				if(isEditable) {
-					label.addStyleName("editHighlight");
-				}
-			}
-		});
-		label.addMouseOutHandler(new MouseOutHandler() {
-			public void onMouseOut(MouseOutEvent event) {
-				label.removeStyleName("editHighlight");
-			}
-		});
-		initWidget(panel);
-	}
-	
-	public void displayEditControls() {
-		panel.remove(label);
-		HorizontalPanel editPanel = new HorizontalPanel();
-		if(editableStyleName != null) {
-			editPanel.addStyleName(editableStyleName);
-		}
-		final TextBox textBox = new TextBox();
-		textBox.setText(label.getText());
-		textBox.setWidth("20em");
-		final Anchor submit = new Anchor("Save");
-		submit.addStyleName("anchorButton");
-		submit.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				ValueChangeEvent.fire(EditableLabel.this, textBox.getText());
-			}
-		});
-		final Anchor cancel = new Anchor("Cancel");
-		submit.addStyleName("anchorButton");
-		cancel.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				cancel();
-			}
-		});
-		editPanel.add(textBox);
-		editPanel.add(submit);
-		editPanel.add(cancel);
-		panel.add(editPanel);
-	}
-	
-	public Label getLabel() {
-		return label;
-	}
-	
-	public void cancel() {
-		panel.clear();
-		panel.add(label);
-	}
-	
-	public void setText(String newValue) {
-		label.setText(newValue);
-		cancel(); // where "cancel" means "no longer editing"
-	}
-	
-	public String getText() {
-		return label.getText();
-	}
-	
-	
-	public boolean isEditable() {
-		return isEditable;
-	}
+    HorizontalPanel panel;
+    Label           label;
+    boolean         isEditable = true;
+    String          editableStyleName;
 
-	public void setEditable(boolean isEditable) {
-		label.setTitle("");
-		this.isEditable = isEditable;
-	}
+    public EditableLabel(String text) {
+        super();
+        panel = new HorizontalPanel();
+        label = new Label(text);
+        label.setTitle("Click to edit");
+        panel.add(label);
+        label.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                if (isEditable) {
+                    label.removeStyleName("editHighlight");
+                    displayEditControls();
+                }
+            }
+        });
+        label.addMouseOverHandler(new MouseOverHandler() {
+            public void onMouseOver(MouseOverEvent event) {
+                if (isEditable) {
+                    label.addStyleName("editHighlight");
+                }
+            }
+        });
+        label.addMouseOutHandler(new MouseOutHandler() {
+            public void onMouseOut(MouseOutEvent event) {
+                label.removeStyleName("editHighlight");
+            }
+        });
+        initWidget(panel);
+    }
 
-	public HandlerRegistration addValueChangeHandler(ValueChangeHandler<String> handler) {
-		return addHandler(handler, ValueChangeEvent.getType());
-	}
+    public void displayEditControls() {
+        panel.remove(label);
+        HorizontalPanel editPanel = new HorizontalPanel();
+        if (editableStyleName != null) {
+            editPanel.addStyleName(editableStyleName);
+        }
+        final TextBox textBox = new TextBox();
+        textBox.setText(label.getText());
+        textBox.setWidth("20em");
+        final Anchor submit = new Anchor("Save");
+        submit.addStyleName("anchorButton");
+        submit.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                ValueChangeEvent.fire(EditableLabel.this, textBox.getText());
+            }
+        });
+        final Anchor cancel = new Anchor("Cancel");
+        submit.addStyleName("anchorButton");
+        cancel.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                cancel();
+            }
+        });
+        textBox.addKeyUpHandler(new KeyUpHandler() {
+            public void onKeyUp(KeyUpEvent event) {
+                if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+                    ValueChangeEvent.fire(EditableLabel.this, textBox.getText());
+                }
+            }
+        });
+        editPanel.add(textBox);
+        editPanel.add(submit);
+        editPanel.add(cancel);
+        panel.add(editPanel);
+        DeferredCommand.addCommand(new Command() {
+            public void execute() {
+                textBox.setFocus(true);
+                textBox.selectAll();
+            }
+        });
+    }
 
-	public String getEditableStyleName() {
-		return editableStyleName;
-	}
+    public Label getLabel() {
+        return label;
+    }
 
-	public void setEditableStyleName(String editableStyleName) {
-		this.editableStyleName = editableStyleName;
-	}
+    public void cancel() {
+        panel.clear();
+        panel.add(label);
+    }
+
+    public void setText(String newValue) {
+        label.setText(newValue);
+        cancel(); // where "cancel" means "no longer editing"
+    }
+
+    public String getText() {
+        return label.getText();
+    }
+
+    public boolean isEditable() {
+        return isEditable;
+    }
+
+    public void setEditable(boolean isEditable) {
+        label.setTitle("");
+        this.isEditable = isEditable;
+    }
+
+    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<String> handler) {
+        return addHandler(handler, ValueChangeEvent.getType());
+    }
+
+    public String getEditableStyleName() {
+        return editableStyleName;
+    }
+
+    public void setEditableStyleName(String editableStyleName) {
+        this.editableStyleName = editableStyleName;
+    }
 }
