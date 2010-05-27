f2b74055616502854354545fb0d4c7f3daffcd2b
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-05-27 19:32:40 +0000

    should fix MMDB-832. queries that request a user's password should now be rejected; queries that could return the user's password are filtered so no rows or triples containing the password are returned.
    the general capability for blacklisting a predicate is provided as well.
    
    we also shouldn't be returning bean mapping code to the user in a metadata export, but that's another issue.

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/TupeloServlet.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/TupeloServlet.java
@@ -40,6 +40,7 @@ package edu.illinois.ncsa.mmdb.web.server;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.servlet.ServletException;
@@ -50,18 +51,22 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.tupeloproject.kernel.Context;
 import org.tupeloproject.kernel.FilterContext;
+import org.tupeloproject.kernel.NotPermittedException;
 import org.tupeloproject.kernel.Operator;
 import org.tupeloproject.kernel.OperatorException;
 import org.tupeloproject.kernel.SparqlOperator;
+import org.tupeloproject.kernel.Transformer;
 import org.tupeloproject.kernel.TripleReader;
 import org.tupeloproject.kernel.Unifier;
 import org.tupeloproject.rdf.Resource;
 import org.tupeloproject.rdf.Triple;
+import org.tupeloproject.rdf.query.Pattern;
 import org.tupeloproject.server.HttpTupeloServlet;
 import org.tupeloproject.util.ListTable;
 import org.tupeloproject.util.Table;
 import org.tupeloproject.util.Tuple;
 import org.tupeloproject.util.UnicodeTranscoder;
+import org.tupeloproject.util.Variable;
 
 import edu.illinois.ncsa.mmdb.web.rest.AuthenticatedServlet;
 
@@ -69,108 +74,179 @@ import edu.illinois.ncsa.mmdb.web.rest.AuthenticatedServlet;
  * TupeloServlet
  */
 public class TupeloServlet extends HttpTupeloServlet {
-    Log log = LogFactory.getLog(TupeloServlet.class);
+    Log                log = LogFactory.getLog(TupeloServlet.class);
+    static Set<String> predicatePrefixBlacklist;
 
-    class LiteralFilter extends FilterContext {
-    	public LiteralFilter(Context c) {
-    		super(c);
-    	}
-    	Resource postProcess(Resource literal) {
-    		if(literal == null) {
-    			return null;
-    		} else if(!literal.isLiteral()) {
-    			return literal;
-    		} else {
-    			String text = literal.getString();
-    			if(!text.matches("[\\P{Cc}\\p{Space}]*")) {
-    				log.warn("warning: bad literal \""+text+"\", escaping");
-    				return Resource.literal(UnicodeTranscoder.encode(text));
-    			} else {
-    				return literal;
-    			}
-    		}
-    	}
-    	void postProcess(TripleReader tr) {
-    		Set<Triple> newResult = new HashSet<Triple>();
-    		for(Triple t : tr.getResult()) {
-    			Triple newT = new Triple();
-    			for(int i = 0; i < 3; i++) {
-    				newT.set(i, postProcess(t.get(i)));
-    			}
-    			newResult.add(newT);
-    		}
-    		tr.setResult(newResult);
-    	}
-    	void postProcess(Unifier u) {
-    		Table<Resource> uResult = u.getResult();
-    		ListTable<Resource> newResult = new ListTable<Resource>();
-    		newResult.setColumnNames(uResult.getColumnNames());
-    		for(Tuple<Resource> row : u.getResult()) {
-    			Tuple<Resource> newRow = new Tuple<Resource>(row.size());
-    			for(int i = 0; i < row.size(); i++) {
-    				newRow.set(i, postProcess(row.get(i)));
-    			}
-    			newResult.addRow(newRow);
-    		}
-    		u.setResult(newResult);
-    	}
-    	public void delegateOperation(Operator operator) throws OperatorException {
-    		getContext().perform(operator);
-    		if(operator instanceof TripleReader) {
-    			postProcess((TripleReader)operator);
-    		} else if(operator instanceof Unifier) {
-    			postProcess((Unifier)operator);
-    		} else if(operator instanceof SparqlOperator) {
-    			log.warn("warning: literal post-processing of SPARQL operators (MMDB-315) not yet implemented!");
-    		}
-    	}
+    class MediciFilter extends FilterContext {
+
+        boolean isBlacklisted(Resource predicate) {
+            if (predicate == null) {
+                return false;
+            }
+            if (predicatePrefixBlacklist == null) {
+                predicatePrefixBlacklist = new HashSet<String>();
+                predicatePrefixBlacklist.add("http://cet.ncsa.uiuc.edu/2007/foaf/context/");
+            }
+            for (String prefix : predicatePrefixBlacklist ) {
+                if (predicate.getString().startsWith(prefix)) {
+                    log.warn(predicate + " is blacklisted");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        boolean isBlacklisted(Triple t) {
+            for (Resource term : t ) {
+                if (isBlacklisted(term)) {
+                    log.warn(t + " is blacklisted");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public MediciFilter(Context c) {
+            super(c);
+        }
+
+        Resource postProcess(Resource literal) {
+            if (literal == null) {
+                return null;
+            } else if (!literal.isLiteral()) {
+                return literal;
+            } else {
+                String text = literal.getString();
+                if (!text.matches("[\\P{Cc}\\p{Space}]*")) {
+                    log.warn("warning: bad literal \"" + text + "\", escaping");
+                    return Resource.literal(UnicodeTranscoder.encode(text));
+                } else {
+                    return literal;
+                }
+            }
+        }
+
+        Triple postProcess(Triple t) {
+            Triple newT = new Triple();
+            for (int i = 0; i < 3; i++ ) {
+                newT.set(i, postProcess(t.get(i)));
+            }
+            return newT;
+        }
+
+        void postProcess(TripleReader tr) {
+            Set<Triple> newResult = new HashSet<Triple>();
+            for (Triple t : tr.getResult() ) {
+                if (!isBlacklisted(t)) {
+                    newResult.add(postProcess(t));
+                }
+            }
+            tr.setResult(newResult);
+        }
+
+        void postProcess(Unifier u) {
+            // find unbound predicate columns; these may carry blacklisted predicates in the result set
+            Set<String> unboundPredicateColumns = new HashSet<String>();
+            for (Pattern p : u.getPatterns() ) {
+                Variable<Resource> predicateTerm = p.get(1);
+                if (!predicateTerm.isBound()) {
+                    unboundPredicateColumns.add(predicateTerm.getName());
+                }
+            }
+            Table<Resource> uResult = u.getResult();
+            ListTable<Resource> newResult = new ListTable<Resource>();
+            newResult.setColumnNames(uResult.getColumnNames());
+            for (Tuple<Resource> row : u.getResult() ) {
+                Tuple<Resource> newRow = new Tuple<Resource>(row.size());
+                boolean rowBlacklisted = false;
+                for (int i = 0; i < row.size(); i++ ) {
+                    if (unboundPredicateColumns.contains(uResult.getColumnNames().get(i)) &&
+                            isBlacklisted(row.get(i))) {
+                        rowBlacklisted = true;
+                        log.warn(row + " is blacklisted");
+                    } else {
+                        newRow.set(i, postProcess(row.get(i)));
+                    }
+                }
+                if (!rowBlacklisted) {
+                    newResult.addRow(newRow);
+                }
+            }
+            u.setResult(newResult);
+        }
+
+        void preProcess(List<Pattern> patterns) throws NotPermittedException {
+            for (Pattern p : patterns ) {
+                Variable<Resource> predicateTerm = p.get(1);
+                if (predicateTerm.isBound() &&
+                        isBlacklisted(predicateTerm.getValue())) {
+                    throw new NotPermittedException("query on blacklisted predicate not permitted");
+                }
+            }
+        }
+
+        public void delegateOperation(Operator operator) throws OperatorException {
+            if (operator instanceof Unifier) {
+                preProcess(((Unifier) operator).getPatterns());
+            } else if (operator instanceof Transformer) {
+                preProcess(((Transformer) operator).getInPatterns());
+            }
+            getContext().perform(operator);
+            if (operator instanceof TripleReader) {
+                postProcess((TripleReader) operator);
+            } else if (operator instanceof Unifier) {
+                postProcess((Unifier) operator);
+            } else if (operator instanceof SparqlOperator) {
+                log.warn("warning: literal post-processing of SPARQL operators (MMDB-315) not yet implemented!");
+            }
+        }
     }
-    
+
     @Override
     public Context getContext() {
-        Context context = new LiteralFilter(TupeloStore.getInstance().getContext());
-    	return context;
+        Context context = new MediciFilter(TupeloStore.getInstance().getContext());
+        return context;
         //log.info("Tupelo Servlet got context "+context);
         //return context;
     }
 
-	@Override
-	public void doDelete(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		if(!AuthenticatedServlet.doAuthenticate(arg0,arg1,getServletContext())) {
-			return;
-		}
-		super.doDelete(arg0, arg1);
-	}
+    @Override
+    public void doDelete(HttpServletRequest arg0, HttpServletResponse arg1)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        if (!AuthenticatedServlet.doAuthenticate(arg0, arg1, getServletContext())) {
+            return;
+        }
+        super.doDelete(arg0, arg1);
+    }
 
-	@Override
-	public void doGet(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		if(!AuthenticatedServlet.doAuthenticate(arg0,arg1,getServletContext())) {
-			return;
-		}
-		super.doGet(arg0, arg1);
-	}
+    @Override
+    public void doGet(HttpServletRequest arg0, HttpServletResponse arg1)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        if (!AuthenticatedServlet.doAuthenticate(arg0, arg1, getServletContext())) {
+            return;
+        }
+        super.doGet(arg0, arg1);
+    }
 
-	@Override
-	public void doPost(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		if(!AuthenticatedServlet.doAuthenticate(arg0,arg1,getServletContext())) {
-			return;
-		}
-		super.doPost(arg0, arg1);
-	}
+    @Override
+    public void doPost(HttpServletRequest arg0, HttpServletResponse arg1)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        if (!AuthenticatedServlet.doAuthenticate(arg0, arg1, getServletContext())) {
+            return;
+        }
+        super.doPost(arg0, arg1);
+    }
 
-	@Override
-	public void doPut(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		if(!AuthenticatedServlet.doAuthenticate(arg0,arg1,getServletContext())) {
-			return;
-		}
-		super.doPut(arg0, arg1);
-	}
+    @Override
+    public void doPut(HttpServletRequest arg0, HttpServletResponse arg1)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        if (!AuthenticatedServlet.doAuthenticate(arg0, arg1, getServletContext())) {
+            return;
+        }
+        super.doPut(arg0, arg1);
+    }
 }
