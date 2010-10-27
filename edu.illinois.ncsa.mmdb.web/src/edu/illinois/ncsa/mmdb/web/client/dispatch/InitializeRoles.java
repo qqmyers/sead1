770475245ac8c42aa5b4784af93d860afe6c93f4
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-10-27 21:36:29 +0000

    fixed MMDB-924

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/dispatch/InitializeRoles.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/dispatch/InitializeRoles.java
@@ -1,9 +1,7 @@
 package edu.illinois.ncsa.mmdb.web.client.dispatch;
 
-import net.customware.gwt.dispatch.shared.Action;
-
 @SuppressWarnings("serial")
-public class InitializeRoles implements Action<EmptyResult> {
+public class InitializeRoles extends AuthorizedAction<EmptyResult> {
     public InitializeRoles() {
     }
 }
--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/InitializeRolesHandler.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/InitializeRolesHandler.java
@@ -7,10 +7,13 @@ import net.customware.gwt.dispatch.shared.ActionException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.tupeloproject.kernel.OperatorException;
+import org.tupeloproject.rdf.Resource;
 
 import edu.illinois.ncsa.mmdb.web.client.dispatch.EmptyResult;
 import edu.illinois.ncsa.mmdb.web.client.dispatch.InitializeRoles;
 import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
+import edu.uiuc.ncsa.cet.bean.rbac.medici.DefaultRole;
+import edu.uiuc.ncsa.cet.bean.rbac.medici.Permission;
 import edu.uiuc.ncsa.cet.bean.tupelo.rbac.medici.MediciRbac;
 
 public class InitializeRolesHandler implements ActionHandler<InitializeRoles, EmptyResult> {
@@ -19,15 +22,22 @@ public class InitializeRolesHandler implements ActionHandler<InitializeRoles, Em
     @Override
     public EmptyResult execute(InitializeRoles arg0, ExecutionContext arg1) throws ActionException {
         MediciRbac rbac = new MediciRbac(TupeloStore.getInstance().getContext());
-
-        try {
-            log.info("Initializing role-based access control");
-            rbac.intializeDefaultRoles();
-            log.info("Finished initializing role-based access control");
-        } catch (OperatorException x) {
-            throw new ActionException("failed to initialize access control");
+        if (TupeloStore.getInstance().isAllowed(arg0, Permission.EDIT_ROLES)) {
+            try {
+                log.info("Initializing role-based access control");
+                rbac.intializeDefaultRoles();
+                log.info("Adding " + arg0.getUser() + " to Administrator role");
+                Resource userUri = Resource.uriRef(arg0.getUser());
+                Resource adminRoleUri = Resource.uriRef(DefaultRole.ADMINISTRATOR.getUri());
+                rbac.addRole(userUri, adminRoleUri);
+                log.info("Finished initializing role-based access control");
+            } catch (OperatorException x) {
+                throw new ActionException("failed to initialize access control");
+            }
+            return new EmptyResult();
+        } else {
+            throw new ActionException("unauthorized");
         }
-        return new EmptyResult();
     }
 
     @Override
