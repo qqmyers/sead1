1294461041ecbc0afdc9a8eb8746b0817fc50132
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-10-28 18:40:21 +0000

    reverting MMDB-922. will be put back in place once bootstrapping is completed. you will still need to take action to restore access, what is recommended is that you initialize roles and permissions on the admin page!

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/HasPermissionHandler.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/HasPermissionHandler.java
@@ -83,8 +83,14 @@ public class HasPermissionHandler implements
         for (Permission permission : action.getPermissions() ) {
             Resource permissionUri = Resource.uriRef(permission.getUri());
             try {
+                log.debug("Checking if user " + userUri + " has permission " + permission.getLabel());
                 boolean hasPermission = false;
-                hasPermission = rbac.checkPermission(userUri, objectUri, permissionUri);
+                if (userUri.getString().endsWith("admin")) { // FIXME
+                    log.debug("User is admin, automatically has permission. FIXME!!!!!!"); // FIXME
+                    hasPermission = true;
+                } else {
+                    hasPermission = rbac.checkPermission(userUri, objectUri, permissionUri);
+                }
                 result.setIsPermitted(permission, hasPermission);
                 log.debug("User " + userUri + " " + (hasPermission ? "has" : "does not have") + " permission " + permission.getLabel());
             } catch (RBACException e) {
