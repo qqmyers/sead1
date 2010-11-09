8a1bb18d93bfcbf8a263240fb94bf12f38bb5fcc
Joe Futrelle <futrelle@ncsa.illinois.edu>
2010-11-09 19:47:08 +0000

    new dispatch handlers for get/set relationship, which should be compatible with the existing action and result classes.
    these are *not* tested and they have not yet been registered in the handler config.

---
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/GetRelationshipHandlerNew.java
@@ -0,0 +1,61 @@
+package edu.illinois.ncsa.mmdb.web.server.dispatch;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.ActionException;
+
+import org.tupeloproject.kernel.OperatorException;
+import org.tupeloproject.kernel.Unifier;
+import org.tupeloproject.rdf.Resource;
+import org.tupeloproject.rdf.terms.Rdf;
+import org.tupeloproject.util.Tuple;
+
+import edu.illinois.ncsa.mmdb.web.client.dispatch.GetRelationship;
+import edu.illinois.ncsa.mmdb.web.client.dispatch.GetRelationshipResult;
+import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
+import edu.uiuc.ncsa.cet.bean.DatasetBean;
+import edu.uiuc.ncsa.cet.bean.tupelo.mmdb.MMDB;
+
+public class GetRelationshipHandlerNew implements ActionHandler<GetRelationship, GetRelationshipResult> {
+
+    @Override
+    public GetRelationshipResult execute(GetRelationship action, ExecutionContext arg1) throws ActionException {
+        try {
+            Resource subject = Resource.uriRef(action.getDatasetURI());
+
+            Unifier u = new Unifier();
+            u.setColumnNames("type", "dataset");
+            u.addPattern("type", Rdf.TYPE, MMDB.USER_RELATIONSHIP); // only fetch user relationship triples
+            u.addPattern(subject, "type", "dataset"); // determine the target dataset uri from the relationship triple
+            // don't fetch the reified stuff (the date and creator of the relationship) because these are not returned by this dispatch
+
+            TupeloStore.getInstance().getContext().perform(u);
+
+            List<DatasetBean> rt = new LinkedList<DatasetBean>();
+            List<String> types = new LinkedList<String>();
+
+            for (Tuple<Resource> row : u.getResult() ) {
+                DatasetBean db = TupeloStore.fetchDataset(row.get(1)); // dbu's only take strings
+                String type = row.get(0).getString();
+                rt.add(db);
+                types.add(type);
+            }
+
+            return new GetRelationshipResult(rt, types);
+        } catch (OperatorException x) {
+            throw new ActionException("get relationships failed", x);
+        }
+    }
+
+    @Override
+    public Class<GetRelationship> getActionType() {
+        return GetRelationship.class;
+    }
+
+    @Override
+    public void rollback(GetRelationship arg0, GetRelationshipResult arg1, ExecutionContext arg2) throws ActionException {
+    }
+}
---
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/dispatch/SetRelationshipHandlerNew.java
@@ -0,0 +1,71 @@
+package edu.illinois.ncsa.mmdb.web.server.dispatch;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
+
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.ActionException;
+
+import org.tupeloproject.kernel.Context;
+import org.tupeloproject.kernel.OperatorException;
+import org.tupeloproject.kernel.TripleWriter;
+import org.tupeloproject.rdf.Namespaces;
+import org.tupeloproject.rdf.Reification;
+import org.tupeloproject.rdf.Resource;
+import org.tupeloproject.rdf.Triple;
+import org.tupeloproject.rdf.terms.Dc;
+import org.tupeloproject.util.Tuple;
+
+import edu.illinois.ncsa.mmdb.web.client.dispatch.SetRelationship;
+import edu.illinois.ncsa.mmdb.web.client.dispatch.SetRelationshipResult;
+import edu.illinois.ncsa.mmdb.web.server.TupeloStore;
+
+public class SetRelationshipHandlerNew implements ActionHandler<SetRelationship, SetRelationshipResult> {
+
+    @Override
+    public SetRelationshipResult execute(SetRelationship action, ExecutionContext exc) throws ActionException {
+        // Here we interpret the "type" field of the action as the URI of the predicate to set
+        try {
+            Context c = TupeloStore.getInstance().getContext();
+            //
+            // we're calling new URI below, to validate that these are all URI's.
+            Resource source = Resource.uriRef(new URI(action.getUri1()));
+            Resource target = Resource.uriRef(new URI(action.getUri2()));
+            Resource predicate = Resource.uriRef(new URI(action.getType()));
+            Resource creator = Resource.uriRef(new URI(action.getCreator()));
+            //
+            // construct a relationship triple, and its inverse
+            Resource inverse = null;
+            for (Triple t : c.match(predicate, Resource.uriRef(Namespaces.owl("inverseOf")), null) ) {
+                inverse = t.getObject();
+            }
+            if (inverse == null) {
+                throw new ActionException("no inverse predicate found for relationship " + predicate);
+            }
+            TripleWriter tw = new TripleWriter();
+            for (Triple relationshipTriple : new Tuple<Triple>(new Triple(source, predicate, target), new Triple(target, inverse, source)) ) {
+                tw.add(relationshipTriple);
+                Resource reified = Resource.uriRef();
+                tw.addAll(Reification.reify(reified, relationshipTriple));
+                tw.add(reified, Dc.CREATOR, creator);
+                tw.add(reified, Dc.DATE, new Date());
+            }
+        } catch (OperatorException x) {
+            throw new ActionException("set relationship failed", x);
+        } catch (URISyntaxException e) {
+            throw new ActionException("set relationship failed", e);
+        }
+        return null;
+    }
+
+    @Override
+    public Class<SetRelationship> getActionType() {
+        return SetRelationship.class;
+    }
+
+    @Override
+    public void rollback(SetRelationship arg0, SetRelationshipResult arg1, ExecutionContext arg2) throws ActionException {
+    }
+}
