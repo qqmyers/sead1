0f1f31cb8d33a6f90f8138b6313ff5cc8ad95675
Rob Kooper <kooper@illinois.edu>
2011-02-09 22:13:16 +0000

    store handlemanager (eventbus)

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/Page.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/Page.java
@@ -43,6 +43,7 @@ package edu.illinois.ncsa.mmdb.web.client.ui;
 
 import net.customware.gwt.dispatch.client.DispatchAsync;
 
+import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -50,85 +51,98 @@ import com.google.gwt.user.client.ui.SimplePanel;
 
 /**
  * @author Luigi Marini
- *
+ * 
  */
 public abstract class Page extends Composite {
 
-	protected final FlowPanel mainLayoutPanel;
-	protected final TitlePanel pageTitle;
-	protected DispatchAsync dispatchAsync;
-	private final SimplePanel feedbackPanel;
+    protected final FlowPanel  mainLayoutPanel;
+    protected final TitlePanel pageTitle;
+    protected DispatchAsync    dispatchAsync;
+    protected HandlerManager   eventBus;
+    private final SimplePanel  feedbackPanel;
 
-	/**
+    /**
 	 * 
 	 */
-	public Page() {
-		mainLayoutPanel = new FlowPanel();
-		mainLayoutPanel.addStyleName("page");
-		initWidget(mainLayoutPanel);
-		
-		// page title
-		pageTitle =  new TitlePanel("Page");
-		mainLayoutPanel.add(pageTitle);
-		
-		// feedback panel
-		feedbackPanel = new SimplePanel();
-		mainLayoutPanel.add(feedbackPanel);
-		
-		layout();
-	}
-	
-	public Page(DispatchAsync dispatchAsync) {
-		this();
-		this.dispatchAsync = dispatchAsync;
-	}
-	
-	/**
-	 * 
-	 * @param title
-	 */
-	public Page(String title, DispatchAsync dispatchAsync) {
-		this(dispatchAsync);
-		setPageTitle(title);
-	}
-	
-	/**
-	 * 
-	 * @param title
-	 */
-	public void setPageTitle(String title) {
-		pageTitle.setText(title);
-	}
-	
-	/**
-	 * 
-	 */
-	public void clear() {
-		mainLayoutPanel.clear();
-		mainLayoutPanel.add(pageTitle);
-		mainLayoutPanel.add(feedbackPanel);
-	}
-	
-	/**
-	 * 
-	 */
-	public void refresh() {
-		layout();
-	}
+    public Page() {
+        this(null, null, null);
+    }
 
-	/**
+    public Page(DispatchAsync dispatchAsync) {
+        this(null, dispatchAsync, null);
+    }
+
+    /**
+     * 
+     * @param title
+     */
+    public Page(String title, DispatchAsync dispatchAsync) {
+        this(title, dispatchAsync, null);
+    }
+
+    /**
+     * 
+     * @param title
+     */
+    public Page(String title, DispatchAsync dispatchAsync, HandlerManager eventBus) {
+        this.dispatchAsync = dispatchAsync;
+        this.eventBus = eventBus;
+
+        mainLayoutPanel = new FlowPanel();
+        mainLayoutPanel.addStyleName("page");
+        initWidget(mainLayoutPanel);
+
+        // page title
+        pageTitle = new TitlePanel("Page");
+        if (title != null) {
+            pageTitle.setText(title);
+        }
+        mainLayoutPanel.add(pageTitle);
+
+        // feedback panel
+        feedbackPanel = new SimplePanel();
+        mainLayoutPanel.add(feedbackPanel);
+
+        layout();
+    }
+
+    /**
+     * 
+     * @param title
+     */
+    public void setPageTitle(String title) {
+        pageTitle.setText(title);
+    }
+
+    /**
 	 * 
 	 */
-	public abstract void layout();
-	
-	/**
+    public void clear() {
+        mainLayoutPanel.clear();
+        mainLayoutPanel.add(pageTitle);
+        mainLayoutPanel.add(feedbackPanel);
+    }
+
+    /**
 	 * 
-	 * @param message
 	 */
-	public void showFeedbackMessage(String message) {
-		Label messageLabel = new Label(message);
-		messageLabel.addStyleName("feedbackMessage");
-		feedbackPanel.clear();
-		feedbackPanel.add(messageLabel);
-	}
+    public void refresh() {
+        layout();
+    }
+
+    /**
+	 * 
+	 */
+    public abstract void layout();
+
+    /**
+     * 
+     * @param message
+     */
+    public void showFeedbackMessage(String message) {
+        Label messageLabel = new Label(message);
+        messageLabel.addStyleName("feedbackMessage");
+        feedbackPanel.clear();
+        feedbackPanel.add(messageLabel);
+    }
 }
