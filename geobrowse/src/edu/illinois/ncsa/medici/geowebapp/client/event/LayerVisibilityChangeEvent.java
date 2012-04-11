1b0853579b18f34fd0206ec3516a929cfa4d7db0
Jong Sung Lee <jonglee@ncsa.uiuc.edu>
2012-04-11 16:23:41 +0000

    added initial version of layer switcher with visibility and opacity controls

---
+++ geobrowse/src/edu/illinois/ncsa/medici/geowebapp/client/event/LayerVisibilityChangeEvent.java
@@ -0,0 +1,36 @@
+package edu.illinois.ncsa.medici.geowebapp.client.event;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class LayerVisibilityChangeEvent extends
+		GwtEvent<LayerVisibilityChangeHandler> {
+
+	public static Type<LayerVisibilityChangeHandler> TYPE = new Type<LayerVisibilityChangeHandler>();
+
+	private String layerName;
+	private boolean visibility = true;
+
+	public LayerVisibilityChangeEvent(String layerName, boolean visibility) {
+		this.layerName = layerName;
+		this.visibility = visibility;
+	}
+
+	@Override
+	public Type<LayerVisibilityChangeHandler> getAssociatedType() {
+		return TYPE;
+	}
+
+	@Override
+	protected void dispatch(LayerVisibilityChangeHandler handler) {
+		handler.onLayerVisibilityChanged(this);
+	}
+
+	public String getLayerName() {
+		return layerName;
+	}
+
+	public boolean getVisibility() {
+		return visibility;
+	}
+
+}
