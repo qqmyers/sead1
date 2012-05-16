451747097f9fb40bb0623f4232e11d74fff583b3
Luigi Marini <lmarini@illinois.edu>
2012-05-16 14:24:30 +0000

    Preliminary code to query for all datasets in a specific tag using resteasy.

---
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/resteasy/ResteasyApplication.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.mmdb.web.server.resteasy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Luigi Marini <lmarini@ncsa.illinois.edu>
+ * 
+ */
+public class ResteasyApplication extends Application {
+
+    private final Set<Object> singletons = new HashSet<Object>();
+
+    public ResteasyApplication() {
+        singletons.add(new TagsRestService());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+}
