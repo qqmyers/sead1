4cf3f7493065c5ff4de2e7359ef7a385b5004c78
Nicholas Tenczar <ntenczar@ncsa.illinois.edu>
2012-08-08 18:32:49 +0000

    Made changes to ptm preview widget. The widget now resizes ptms that are larger that 600 by 600, otherwise it preserves the original dimensions.

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/preview/PreviewPTMBeanWidget.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/preview/PreviewPTMBeanWidget.java
@@ -3,6 +3,8 @@
  */
 package edu.illinois.ncsa.mmdb.web.client.ui.preview;
 
+import java.math.BigDecimal;
+
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.ui.HTML;
 
@@ -14,6 +16,9 @@ import edu.uiuc.ncsa.cet.bean.PreviewPTMBean;
  * 
  */
 public class PreviewPTMBeanWidget extends PreviewBeanWidget<PreviewPTMBean> {
+    private static final int    MAX_WIDTH    = 600;
+    private static final int    MAX_HEIGHT   = 600;
+
     private static final String ANCHOR_TEXT  = "Polynomial Texture Map";
     private static final String DOWNLOAD_URL = "api/image/";
 
@@ -42,37 +47,29 @@ public class PreviewPTMBeanWidget extends PreviewBeanWidget<PreviewPTMBean> {
             return;
         }
 
+        long height = getPreviewBean().getHeight();
+        long width = getPreviewBean().getWidth();
+
+        if (height > MAX_HEIGHT || width > MAX_WIDTH) {
+            BigDecimal max = new BigDecimal(MAX_WIDTH);
+            BigDecimal original = new BigDecimal(Math.max(height, width));
+
+            BigDecimal quotient = max.divide(original, BigDecimal.ROUND_HALF_UP);
+
+            height = quotient.multiply(new BigDecimal(height)).longValue();
+            width = quotient.multiply(new BigDecimal(width)).longValue();
+        }
+
         StringBuilder sb = new StringBuilder();
 
-        sb.append("<object archive=\"plugins/envlib.jar\" classid=\"java:jpview/gui/Standalone.class\" type=\"application/x-java-applet\" width=\"400\" height=\"400\">");
-        sb.append(createParam("archive", "plugins/envlib.jar"));
-        sb.append(createParam("ptmfile", "../../" + DOWNLOAD_URL + getPreviewBean().getUri()));
-        sb.append(createParam("bg_red", "100"));
-        sb.append(createParam("bg_green", "100"));
-        sb.append(createParam("bg_blue", "100"));
-        sb.append(createParam("pw", "400"));
-        sb.append(createParam("ph", "400"));
-
-        sb.append("<object classid=\"clsid:8AD9C840-044E-11D1-B3E9-00805F499D93\" codebase=\"http://java.sun.com/update/1.6.0/jinstall-6u20-windows-i586.cab\" width=\"400\" height=\"400\" />");
-        sb.append(createParam("code", "Standalone.class"));
-        sb.append(createParam("archive", "plugins/envlib.jar"));
-        sb.append(createParam("ptmfile", "../../" + DOWNLOAD_URL + getPreviewBean().getUri()));
-        sb.append(createParam("bg_red", "100"));
-        sb.append(createParam("bg_green", "100"));
-        sb.append(createParam("bg_blue", "100"));
-        sb.append(createParam("pw", "400"));
-        sb.append(createParam("ph", "400"));
-
-        sb.append("</object></object>");
-
         sb = new StringBuilder();
-        sb.append("<applet archive=\"plugins/envlib.jar\" code=\"jpview/gui/Standalone.class\" height=\"400\" width=\"400\">");
+        sb.append("<applet archive=\"plugins/envlib.jar\" code=\"jpview/gui/Standalone.class\" height=\"" + height + "\" width=\"" + width + "\">");
         sb.append(createParam("ptmfile", "../../" + DOWNLOAD_URL + getPreviewBean().getUri()));
         sb.append(createParam("bg_red", "100"));
         sb.append(createParam("bg_green", "100"));
         sb.append(createParam("bg_blue", "100"));
-        sb.append(createParam("pw", "400"));
-        sb.append(createParam("ph", "400"));
+        sb.append(createParam("pw", width + ""));
+        sb.append(createParam("ph", height + ""));
         sb.append("</applet>");
 
         panel.setHTML(sb.toString());
