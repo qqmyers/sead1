e4639ae251b9b2807569d69c7d33204a76fc6a32
Jong Sung Lee <jonglee@ncsa.uiuc.edu>
2013-02-06 22:05:38 +0000

    added getTags that will return all tags

--- geobrowse/src/edu/illinois/ncsa/medici/geowebapp/server/MediciRestUtil.java
+++ geobrowse/src/edu/illinois/ncsa/medici/geowebapp/server/MediciRestUtil.java
@@ -1,29 +1,31 @@
 package edu.illinois.ncsa.medici.geowebapp.server;
 
 import java.io.IOException;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import sun.misc.BASE64Encoder;
 
 public class MediciRestUtil {
 	public static String TAG_URL = "http://sead.ncsa.illinois.edu/resteasy/tags";
 
-	public static String getUriByTag(String tag) {
+	public static List<String> getTags() {
+		List<String> tags = new ArrayList<String>();
 		DefaultHttpClient httpclient = new DefaultHttpClient();
 		try {
-			HttpGet httpget = new HttpGet(TAG_URL + "/" + tag + "/datasets");
+			HttpGet httpget = new HttpGet(TAG_URL);
 			String u = "";
 			String p = "";
-			String userPassString = u+":"+p;
+			String userPassString = u + ":" + p;
 			BASE64Encoder b = new BASE64Encoder();
 
 			httpget.addHeader("Authorization",
@@ -35,23 +37,66 @@ public class MediciRestUtil {
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
 			String responseBody = httpclient.execute(httpget, responseHandler);
 			System.out.println(responseBody);
+			JSONArray jsArray = new JSONArray(responseBody);
+			for (int i = 0; i < jsArray.length(); i++) {
+				String tag = jsArray.getString(i);
+				tags.add(tag);
+			}
 
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-
+		} catch (JSONException e) {
+			e.printStackTrace();
 		} finally {
-			// When HttpClient instance is no longer needed,
-			// shut down the connection manager to ensure
-			// immediate deallocation of all system resources
 			httpclient.getConnectionManager().shutdown();
 		}
 
-		return null;
+		return tags;
+	}
+
+	public static List<String> getUrisByTag(String tag) {
+		List<String> uris = new ArrayList<String>();
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+		try {
+			HttpGet httpget = new HttpGet(TAG_URL + "/" + tag + "/datasets");
+			String u = "jonglee1@illinois.edu";
+			String p = "2late";
+			String userPassString = u + ":" + p;
+			BASE64Encoder b = new BASE64Encoder();
+
+			httpget.addHeader("Authorization",
+					"Basic " + b.encode(userPassString.getBytes()));
+
+			System.out.println("executing request" + httpget.getRequestLine());
+			// HttpResponse response = httpclient.execute(httpget);
+
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			String responseBody = httpclient.execute(httpget, responseHandler);
+
+			JSONArray jsArray = new JSONArray(responseBody);
+			for (int i = 0; i < jsArray.length(); i++) {
+				JSONObject js = jsArray.getJSONObject(i);
+				String uri = js.getString("uri");
+				System.out.println(uri);
+				uris.add(uri);
+			}
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			httpclient.getConnectionManager().shutdown();
+		}
+
+		return uris;
 	}
 
 	public static void main(String[] args) {
-		getUriByTag("analysis");
+		getTags();
 	}
 }
