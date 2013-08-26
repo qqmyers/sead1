f932808942e12dfde63275d3023a267059ed851a
Jim Myers <myersjd@umich.edu>
2013-08-26 16:51:28 -0400

    testing git upload - adding a few notes to a package file

--- ACRCommon/src/org/sead/acr/common/package-info.java
+++ ACRCommon/src/org/sead/acr/common/package-info.java
@@ -2,6 +2,8 @@
  * This package collects common code for interacting with the Medici 1.0 component of the ACR 
  * (primarily via SPARQL queries) and creating JSON outputs for common tasks, e.g.
  * listing collections and datasets, retrieving metadata for a given item, etc. 
+ * The MediciProxy class manages credentials and formats the output while DataAccess performs the query calls.
+ * Common queries are stored in the utilities.Queries class 
  */
 /**
  * @author Jim Myers, Ram Prassana, Saurabh Malviya, Rob Kooper
