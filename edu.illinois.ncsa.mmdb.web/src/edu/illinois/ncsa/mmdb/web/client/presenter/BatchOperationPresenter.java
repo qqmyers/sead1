23f7a2c4991f89f84c6e51f05d40791c6f200529
Jim Myers <myersjd@umich.edu>
2013-11-19 09:49:49 -0500

    using dcterms:description for overloaded description/badge(source for preview image)

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/presenter/BatchOperationPresenter.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/presenter/BatchOperationPresenter.java
@@ -388,12 +388,8 @@ public class BatchOperationPresenter extends BasePresenter<BatchOperationPresent
                                 public void onAllowed() {
                                     final String collectionUri = atc.getSelectedValue();
                                     final Set<String> selectedDatasets = new HashSet<String>(sessionState.getSelectedItems());
-<<<<<<< HEAD
                                     //DCTERMS:description is being overloaded - the user message is 'describes collection' but we will also be using this to select a collection preview image 
                                     SetUserMetadata sum = new SetUserMetadata(collectionUri, "http://purl.org/dc/terms/description", selectedDatasets, true);
-=======
-                                    SetUserMetadata sum = new SetUserMetadata(collectionUri, "http://cet.ncsa.uiuc.edu/2007/hasBadge", selectedDatasets, true);
->>>>>>> refs/remotes/origin/sead-1.2
                                     final BatchCompletedEvent done = new BatchCompletedEvent(selectedDatasets.size(), "set");
                                     service.execute(sum,
                                             new AsyncCallback<EmptyResult>() {
