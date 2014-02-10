68624603ade4c7f042d1d43d9d9453b11ea377aa
Rob Kooper <kooper@illinois.edu>
2014-02-09 22:36:44 -0800

    added geoserver logs to thing to remove

---
+++ scripts/acr/cleanvm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# run this as:
+# history -c && ./cleanvm.sh
+
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+
+sudo apt-get --purge remove
+sudo apt-get clean
+sudo rm /etc/udev/rules.d/70-persistent-net.rules
+sudo rm -rf /root/.ssh
+sudo rm /root/.bash_history
+sudo touch /root/.bash_history
+
+sudo rm -rf /var/log /var/lib/tomcat6/webapps/geoserver/data/logs/geoserver.log
+sudo mkdir -p /var/log/{apparmor,apt,dist-upgrade,fsck,installer,landscape,mysql,news,unattended-upgrades,tomcat6}
+sudo chown landscape /var/log/landscape
+sudo chown tomcat6 /var/log/tomcat6
+sudo chown mysql.adm /var/log/mysql
+sudo chmod 710 /var/log/mysql
+
+sudo touch /var/log/wtmp /var/log/btmp
+sudo chown root.utmp /var/log/wtmp /var/log/btmp
+
+sudo rm /etc/ssh/ssh_host*key*
+
+history -c
+rm -rf ${HOME}/{.ssh,.cache,.bash_history,.mysql_history,.sudo_as_admin_successful,.viminfo}
+touch ${HOME}/.bash_history
+
+sudo shutdown -h now
