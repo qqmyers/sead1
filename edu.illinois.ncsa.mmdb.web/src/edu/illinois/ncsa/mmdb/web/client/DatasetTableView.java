37af750cba0f566c7c5f0505d2232e876b42ddaa
Jim Myers <myersjd@umich.edu>
2014-04-02 10:41:58 -0400

    SEAD-165 : cleanup of old/unused classes

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/DatasetTableView.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/DatasetTableView.java
@@ -40,7 +40,5 @@ package edu.illinois.ncsa.mmdb.web.client;
 
 import com.google.gwt.user.client.ui.FlexTable;
 
-import edu.illinois.ncsa.mmdb.web.client.DatasetTablePresenter.Display;
-
 public abstract class DatasetTableView extends FlexTable implements Display {
 }
