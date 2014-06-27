b558fa8335ea514212a915d11f14bd78ced0d2ca
Jim Myers <myersjd@umich.edu>
2014-06-27 12:19:22 -0400

    unused import

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/admin/ConfigurationWidget.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/admin/ConfigurationWidget.java
@@ -3,7 +3,6 @@ package edu.illinois.ncsa.mmdb.web.client.ui.admin;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.UUID;
 
 import net.customware.gwt.dispatch.client.DispatchAsync;
 
