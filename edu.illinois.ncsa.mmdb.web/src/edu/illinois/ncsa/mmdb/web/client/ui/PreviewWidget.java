6db54101e71f669b9088449e809389624b3ab799
Ashwini Vaidya <vaidya10@illinois.edu>
2014-08-01 12:45:32 -0500

    added alternate gray icon based on file type

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/PreviewWidget.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/client/ui/PreviewWidget.java
@@ -91,6 +91,7 @@ public class PreviewWidget extends Composite implements HasAllMouseHandlers {
     public static final Map<String, String> PREVIEW_URL;
     public static final Map<String, String> GRAY_URL;
     public static final Map<String, String> PENDING_URL;
+    public Map<String, String>              ALT_GRAY_URL;
 
     int                                     maxWidth     = 600;
 
@@ -142,7 +143,7 @@ public class PreviewWidget extends Composite implements HasAllMouseHandlers {
 
     /**
      * Create a preview. If the desired size is small (thumbnail) try showing
-     * the thumbnail, if thumbnail notavailable show a no preview label. If the
+     * the thumbnail, if thumbnail not available show a no preview label. If the
      * desired size is large (preview) ask the server for the size of the
      * preview and then properly size the image keeping the correct aspect
      * ratio.
@@ -152,11 +153,11 @@ public class PreviewWidget extends Composite implements HasAllMouseHandlers {
      * @param link
      */
     public PreviewWidget(String datasetUri, String desiredSize, final String link, DispatchAsync dispatchAsync) {
-        this(datasetUri, desiredSize, link, UNKNOWN_TYPE, true, dispatchAsync);
+        this(datasetUri, desiredSize, link, UNKNOWN_TYPE, dispatchAsync);
     }
 
     public PreviewWidget(String datasetUri, String desiredSize, String link, String type, DispatchAsync dispatchAsync) {
-        this(datasetUri, desiredSize, link, type, true, true, dispatchAsync);
+        this(datasetUri, desiredSize, link, type, true, dispatchAsync);
     }
 
     public PreviewWidget(String datasetUri, String desiredSize, String link, String type, final boolean checkPending, DispatchAsync dispatchAsync) {
@@ -176,7 +177,19 @@ public class PreviewWidget extends Composite implements HasAllMouseHandlers {
         //
         changeImage(uri, size, link, type, checkPending, initialDisplay);
         //
+        createDefaultNoPreviewImages(type);
         initWidget(imagePanel);
+    }
+
+    protected void createDefaultNoPreviewImages(String type) {
+        if (type != null && !UNKNOWN_TYPE.equals(type)) {
+            String defaultImageFileName = "./images/defaultpreviews/nopreview-" + type + "-100.gif";
+            ALT_GRAY_URL = new HashMap<String, String>();
+            ALT_GRAY_URL.put(GetPreviews.SMALL, defaultImageFileName);
+            ALT_GRAY_URL.put(GetPreviews.LARGE, defaultImageFileName); // TODO is this correct?
+            ALT_GRAY_URL.put(GetPreviews.BADGE, defaultImageFileName);
+        }
+
     }
 
     public void changeImage(String uri, String sz, String link, String type, boolean checkPending, boolean initialDisplay) {
@@ -317,7 +330,19 @@ public class PreviewWidget extends Composite implements HasAllMouseHandlers {
 
     void showNoPreview(String sz, String link) {
         if (!isNoPreview()) {
-            noPreview = new Image(GRAY_URL.get(sz));
+            if (ALT_GRAY_URL != null) {
+                noPreview = new Image(ALT_GRAY_URL.get(sz));
+                final String siz = sz;
+                //revert back to the GRAY_URL if the nopreview file for this type is not found!
+                noPreview.addErrorHandler(new ErrorHandler() {
+                    @Override
+                    public void onError(ErrorEvent event) {
+                        noPreview.setUrl(GRAY_URL.get(siz));
+                    }
+                });
+            } else {
+                noPreview = new Image(GRAY_URL.get(sz));
+            }
             addLink(noPreview, link);
             setImage(noPreview);
         }
