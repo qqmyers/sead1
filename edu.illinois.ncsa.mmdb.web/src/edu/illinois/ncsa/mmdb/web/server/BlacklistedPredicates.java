2a9df85689b59839d0b1f95ae7acd0972153b203
Ashwini Vaidya <vaidya10@illinois.edu>
2014-12-17 12:45:32 -0600

    oops, set blacklistedPredicates to hold actual list

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/BlacklistedPredicates.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/BlacklistedPredicates.java
@@ -9,21 +9,23 @@ import org.tupeloproject.rdf.terms.Rdfs;
 
 public final class BlacklistedPredicates {
     private BlacklistedPredicates() {
-        blacklistedPredicates.add(Resource.uriRef("http://purl.org/dc/terms/license"));
-        blacklistedPredicates.add(Resource.uriRef("http://purl.org/dc/terms/rightsHolder"));
-        blacklistedPredicates.add(Resource.uriRef("http://purl.org/dc/terms/rights"));
-        blacklistedPredicates.add(Dc.TITLE);
-        blacklistedPredicates.add(Dc.CREATOR);
-        blacklistedPredicates.add(Dc.IDENTIFIER);
-        blacklistedPredicates.add(Dc.CONTRIBUTOR); // should whitelist once we have multi-valued user properties
-        blacklistedPredicates.add(Rdfs.LABEL);
-        // there's an even longer list, but these are some of the ones I expect we'd have the most problems with
-
     }
 
     private static Set<Resource> blacklistedPredicates = new HashSet<Resource>();
 
     public static Set<Resource> GetResources() {
+        if (blacklistedPredicates.isEmpty()) {
+            // there's an even longer list, but these are some of the ones I expect we'd have the most problems with
+            blacklistedPredicates.add(Resource.uriRef("http://purl.org/dc/terms/license"));
+            blacklistedPredicates.add(Resource.uriRef("http://purl.org/dc/terms/rightsHolder"));
+            blacklistedPredicates.add(Resource.uriRef("http://purl.org/dc/terms/rights"));
+            blacklistedPredicates.add(Dc.TITLE);
+            blacklistedPredicates.add(Dc.CREATOR);
+            blacklistedPredicates.add(Dc.IDENTIFIER);
+            blacklistedPredicates.add(Dc.CONTRIBUTOR); // should whitelist once we have multi-valued user properties
+            blacklistedPredicates.add(Rdfs.LABEL);
+
+        }
         return blacklistedPredicates;
     }
 
