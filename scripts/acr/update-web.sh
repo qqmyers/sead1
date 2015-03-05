57695c1df46e1c2f539fea8c70e9de75fa0aeadd
Jim Myers <myersjd@umich.edu>
2015-03-05 12:12:29 -0500

    updating sripts: dashboard, discovery, and geobrowse are now part of main web app, acr.server --> server.properties is the only config file, added new sead-va user with AUTHOR power for use in publishing (temporary until we get per curator google auth going), added redirect from / to /acr replacing the static landing page at /. update-web.sh does cleanup of old dashboard, discovery, geobrowse files/dirs.

--- scripts/acr/update-web.sh
+++ scripts/acr/update-web.sh
@@ -6,12 +6,17 @@ cd /home/medici
 
 rm -rf war medici.war /var/lib/tomcat6/webapps/acr
 
+#One-time cleanup from old versions
+rm -rf dashboard.log4j dashboard.properties dashboard.war update-dashboard.sh /var/lib/tomcat6/webapps/dashboard
+rm -rf discovery.log4j discovery.properties discovery.war update-discovery.sh /var/lib/tomcat6/webapps/discovery
+rm -rf geobrowse.log4j geobrowse.properties geobrowse.war update-geobrowse.sh /var/lib/tomcat6/webapps/geobrowse
+
 wget -q -O medici.war https://opensource.ncsa.illinois.edu/bamboo/browse/MMDB-MEDICI/latestSuccessful/artifact/JOB1/medici.war/medici.war
 
 unzip -q -d war medici.war
 cp acr.log4j  war/WEB-INF/classes/log4j.properties
 cp acr.server war/WEB-INF/classes/server.properties
-cp acr.common war/WEB-INF/classes/acr_commons.properties
+
 
 chown -R tomcat6.tomcat6 war
 mv war /var/lib/tomcat6/webapps/acr
