5bd9194b72d1429ae4506c33b09377ea2b310433
Jim Myers <myersjd@umich.edu>
2015-03-16 17:06:24 -0400

    Tag cloud impl from Michael Iannaccone <mcianna@umich.edu>

---
+++ edu.illinois.ncsa.mmdb.web/war/js/jquery.tagcanvas.init.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+	setTimeout(function() {
+
+        jQuery('#tagCloud').append('<div id="CanvasContainer"><canvas width="300" height="300" id="tagCanvas"></canvas></div>');
+
+          if(!jQuery('#tagCanvas').tagcanvas({
+            textColour: '#0b5ca3',
+            outlineColour: '#f3882a',
+            reverse: true,
+            depth: 0.8,
+            maxSpeed: 0.05,
+            textHeight: 25,
+            weight: true,
+            weightFrom: 'data-weight'
+          },'weightedtaglist')) {
+   		   console.log('Error: could not create canvas');
+            jQuery('#CanvasContainer').hide();
+          }
+  
+	}, 800);
+     
+	 
+});
