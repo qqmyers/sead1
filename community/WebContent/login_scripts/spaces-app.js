38f23a0e24bcb16e3643e5dcf9e2c61ae6aa0f4d
Michael Iannaccone <mikeiannaccone@yahoo.com>
2015-11-18 14:54:27 -0500

    improve loading performance by allow slow-loading spaces to load individually after initial page load

--- community/WebContent/login_scripts/spaces-app.js
+++ community/WebContent/login_scripts/spaces-app.js
@@ -1,9 +1,16 @@
 var seadSpaces = {};
 
+/*
+Load empty space wrappers based on number of spaces
+Call an init from each wrapper and use .when to load content? 
+ 
+*/
+
+
 seadSpaces.doInfoAjax = function(url, i) {
 	return $.ajax({
 		type : "GET",
-		timeout : '10000',
+		timeout : '400',
 		url : "GetSysInfo",
 		data : {
 			server : url
@@ -15,7 +22,7 @@ seadSpaces.doInfoAjax = function(url, i) {
 seadSpaces.doConfigAjax = function(url) {
 	return $.ajax({
 		type : "GET",
-		timeout : '10000',
+		timeout : '400',
 		url : "GetProjectInfo",
 		data : {
 			server : url
@@ -137,7 +144,8 @@ seadSpaces.initSort = function() {
 		$('.sort').removeClass('active');
 	});
 
-	$('#loading-spinner').remove();
+	
+	//$('.loading-spinner').remove();
 }
 
 seadSpaces.formatBytes = function(bytes, decimals) {
@@ -154,8 +162,11 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 		projectLogo, projectColor, projectBg, datasets_display, datasets_raw,
 		users, users_raw, views, views_raw, collections, collections_raw,
 		published, bytes, value) {
+		var timeout = false;
 	if (projectName == null) {
-		projectName = value + ' is currently busy or offline';
+		//projectName = value + ' is currently busy or offline';
+		projectName = 'loading...';
+		var timeout = true;
 	}
 
 	if (bytes !== null) {
@@ -173,18 +184,21 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 	var page = '';
 	page += '<div class="span4">';
 	page += '<div class="space-wrapper">';
+	if(timeout == true){page += '<div class="loading-spinner"><i class="fa fa-spinner fa-pulse fa-5x"></i><div class="hidden-space-url">'+ value +'</div></div>';}
 	page += '<a href="' + value + '"><div class="fade-wrapper">';
-	if (projectLogo || projectBg) {
+	if (projectLogo || projectBg || timeout == true) {
 		page += '<div class="fade-out" style="background-image:url('
 				+ projectLogo + '),url(' + projectBg + ');">';
 	} else {
 		page += '<div class="fade-out">';
 	}
 	page += '</div>';
-	if (projectDescription) {
-		page += '<div class="fade-in"><div class="fade-in-content">'
+	if (projectDescription  || timeout == true) {
+		if( timeout == true){page += '<div class="fade-in"><div class="fade-in-content">' + '</div></div>';}
+		else{page += '<div class="fade-in"><div class="fade-in-content">'
 				+ projectDescription.replace(/<(?:.|\n)*?>/gm, '')
 				+ '</div></div>';
+		}
 	}
 	page += '</div></a>';
 	page += '<div class="space-stats">';
@@ -196,7 +210,8 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 		views_raw = 0;
 	}
 	page += '<li class="views_raw">' + views_raw + '</li>';
-	if (views) {
+	if (views || timeout == true) {
+		if(timeout == true){views = '';}
 		page += '<li class="views" title="' + views
 				+ ' views"><i class="fa fa-lg fa-eye"></i> ' + views + '</li>';
 	}
@@ -205,7 +220,8 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 		users_raw = 0;
 	}
 	page += '<li class="users_raw">' + users_raw + '</li>';
-	if (users) {
+	if (users || timeout == true) {
+		if(timeout == true){users = '';}
 		page += '<li class="teammates" title="' + users
 				+ ' contributors"><i class="fa fa-lg fa-user"></i> ' + users
 				+ '</li>';
@@ -217,7 +233,8 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 	page += '<li class="collections_raw" title="' + collections_raw
 			+ ' collections"> '
 			+ collections_raw + '</li>';
-	if (collections) {
+	if (collections || timeout == true) {
+		if(timeout == true){collections = '';}
 		page += '<li class="collections" title="' + collections
 				+ ' collections"><i class="fa fa-lg fa-folder"></i> '
 				+ collections + '</li>';
@@ -227,7 +244,8 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 		datasets_raw = 0;
 	}
 	page += '<li class="datasets_raw">' + datasets_raw + '</li>';
-	if (datasets_display) {
+	if (datasets_display || timeout == true) {
+		if(timeout == true){datasets_display = '';}
 		page += '<li class="datasets" title="' + datasets_display
 				+ ' datasets"><i class="fa fa-lg fa-database"></i> '
 				+ datasets_display + '</li>';
@@ -237,12 +255,13 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 		bytes_raw = 0;
 	}
 	page += '<li class="bytes_raw">' + bytes_raw + '</li>';
-	if (bytes) {
-		page += '<li class="bytes" title="' + bytes
-				+ '"><i class="fa fa-lg fa-hdd-o"></i> ' + bytes + '</li>';
+	if (bytes || timeout == true) {
+		if(timeout == true){bytes = '';}
+		page += '<li class="bytes" title="' + bytes + '"><i class="fa fa-lg fa-hdd-o"></i> ' + bytes + '</li>';
 	}
 
-	if (published) {
+	if (published || timeout == true) {
+		if(timeout == true){published = '';}
 		page += '<li title="'
 				+ published
 				+ ' published dataset"><i class="fa fa-lg fa-folder-open"></i> <span class="published">'
@@ -254,9 +273,9 @@ seadSpaces.buildGrid = function(size, i, projectName, projectDescription,
 	page += '</div>';
 	$('.project-spaces-dashboard .row-fluid').append(page);
 
-	if (i == size) {
+	/*if (i == size) {
 		seadSpaces.initSort();
-	}
+	}*/
 
 }
 
@@ -420,7 +439,7 @@ seadSpaces.demoSpace = function(){
 			.then(
 					function(config,
 							info) {
-
+						
 						var projectName = config[0]["project.name"];
 						var projectDescription = config[0]["project.description"];
 						var projectLogo = config[0]["project.header.logo"];
@@ -473,9 +492,16 @@ seadSpaces.demoSpace = function(){
 						
 						
 					
+					
+						
 						if (projectName == null) {
-							projectName = value + ' is currently busy or offline';
+							//projectName = value + ' is currently busy or offline';
+							projectName = 'loading...';
+							var timeout = true;
 						}
+						
+						
+						
 
 						if (bytes !== null) {
 							if (bytes.indexOf('GB') > 0 || bytes.indexOf('bytes') > 0
@@ -492,9 +518,10 @@ seadSpaces.demoSpace = function(){
 						var page = '';
 					
 						page += '<div class="space-wrapper">';
+						if(timeout == true){page += '<div class="loading-spinner"><i class="fa fa-spinner fa-pulse fa-5x"></i><div class="hidden-space-url">'+ value +'</div></div>';}
 						page += '<a href="' + value + '"><div class="fade-wrapper">';
 						
-						if (projectLogo || projectBg) {
+						if (projectLogo || projectBg || timeout == true) {
 							page += '<div class="fade-out" style="background-image:url(' + projectBg + ');">';
 						} else {
 							page += '<div class="fade-out">';
@@ -502,7 +529,9 @@ seadSpaces.demoSpace = function(){
 						page += '<div class="space-stats"><div class="space-name"><img src="' + projectLogo + '" alt="" /><div class="projectName">' + projectName + '</div></div></div>';
 						page += '</div>';
 						
-						if (projectDescription) {
+						if (projectDescription || timeout == true) {
+							if( timeout == true){page += '<div class="fade-in"><div class="fade-in-content">' + '</div></div>';}
+
 							page += '<div class="fade-in"><div class="fade-in-content">'
 									+ projectDescription.replace(/<(?:.|\n)*?>/gm, '')
 									+ '</div></div>';
@@ -515,7 +544,8 @@ seadSpaces.demoSpace = function(){
 							views_raw = 0;
 						}
 						page += '<li class="views_raw">' + views_raw + '</li>';
-						if (views) {
+						if (views || timeout == true) {
+							if(timeout == true){views = '';}
 							page += '<li class="views" title="' + views
 									+ ' views"><i class="fa fa-lg fa-eye"></i> ' + views + '</li>';
 						}
@@ -524,7 +554,8 @@ seadSpaces.demoSpace = function(){
 							users_raw = 0;
 						}
 						page += '<li class="users_raw">' + users_raw + '</li>';
-						if (users) {
+						if (users || timeout == true) {
+							if(timeout == true){users = '';}
 							page += '<li class="teammates" title="' + users
 									+ ' contributors"><i class="fa fa-lg fa-user"></i> ' + users
 									+ '</li>';
@@ -536,7 +567,8 @@ seadSpaces.demoSpace = function(){
 						page += '<li class="collections_raw" title="' + collections_raw
 								+ ' collections"> '
 								+ collections_raw + '</li>';
-						if (collections) {
+						if (collections || timeout == true) {
+							if(timeout == true){collections = '';}
 							page += '<li class="collections" title="' + collections
 									+ ' collections"><i class="fa fa-lg fa-folder"></i> '
 									+ collections + '</li>';
@@ -546,7 +578,8 @@ seadSpaces.demoSpace = function(){
 							datasets_raw = 0;
 						}
 						page += '<li class="datasets_raw">' + datasets_raw + '</li>';
-						if (datasets_display) {
+						if (datasets_display|| timeout == true) {
+							if(timeout == true){datasets_display = '';}
 							page += '<li class="datasets" title="' + datasets_display
 									+ ' datasets"><i class="fa fa-lg fa-database"></i> '
 									+ datasets_display + '</li>';
@@ -556,12 +589,13 @@ seadSpaces.demoSpace = function(){
 							bytes_raw = 0;
 						}
 						page += '<li class="bytes_raw">' + bytes_raw + '</li>';
-						if (bytes) {
-							page += '<li class="bytes" title="' + bytes
-									+ '"><i class="fa fa-lg fa-hdd-o"></i> ' + bytes + '</li>';
+						if (bytes || timeout == true) {
+							if(timeout == true){bytes = '';}
+							page += '<li class="bytes" title="' + bytes + '"><i class="fa fa-lg fa-hdd-o"></i> ' + bytes + '</li>';
 						}
 
-						if (published) {
+						if (published || timeout == true) {
+							if(timeout == true){published = '';}
 							page += '<li title="'
 									+ published
 									+ ' published dataset"><i class="fa fa-lg fa-folder-open"></i> <span class="published">'
@@ -570,16 +604,155 @@ seadSpaces.demoSpace = function(){
 						page += '<ul>';
 						page += '</div>';
 						page += '</div>';
-
+						
 					$('.sead-demo').append(page);
 
 					})
 			.fail(
 					function(response) {
 						
+						page ='';
+						page += '<div class="space-wrapper">';
+						page += '<div class="loading-spinner"><i class="fa fa-spinner fa-pulse fa-5x"></i><div class="hidden-space-url">'+ value +'</div></div>';
+						page += '<a href="https://sead-demo.ncsa.illinois.edu/acr">';
+						page += '<div class="fade-wrapper">';
+						page += '<div class="fade-out" style="background-image:url();">';
+						page += '<div class="space-stats">';
+						page += '<div class="space-name"><img src="https://sead-demo.ncsa.illinois.edu/acr/images/SEAD-small.png" alt="">';
+						page += '<div class="projectName">SEAD Demo Space</div>';
+						page += '</div></div></div>';
+						page += '<div class="fade-in">';
+						page += '<div class="fade-in-content"></div></div></div></a>';
+						page += '<ul>';
+						page += '<li class="views_raw"></li>';
+						page += '<li class="views" title=""><i class="fa fa-lg fa-eye"></i></li>';
+						page += '<li class="users_raw"></li>';
+						page += '<li class="teammates" title=""><i class="fa fa-lg fa-user"></i></li>';
+						page += '<li class="collections_raw" title=""></li>';
+						page += '<li class="collections" title=""><i class="fa fa-lg fa-folder"></i></li>';
+						page += '<li class="datasets_raw"></li>';
+						page += '<li class="datasets" title=""><i class="fa fa-lg fa-database"></i></li>';
+						page += '<li class="bytes_raw"></li>';
+						page += '<li class="bytes" title=""><i class="fa fa-lg fa-hdd-o"></i> </li>';
+						page += '<li title=""><i class="fa fa-lg fa-folder-open"></i> <span class="published"></span></li>';
+						page += '</ul></div>';
+						$('.sead-demo').append(page);
+						
+						
+						
+						
 					});
 
 }
 
+
+seadSpaces.loadSlowSpaces = function(){
+
+	var unloadedSpaces = $('.loading-spinner').length;
+	if(unloadedSpaces > 0) {
+	$('.loading-spinner').each(function() {
+							var spaceWrapper = $(this).parents('.space-wrapper');
+							var value = $(this).find('.hidden-space-url').text();
+							$
+									.when(
+											seadSpaces
+													.doConfigAjax(value),
+											seadSpaces
+													.doInfoAjax(value))
+									.then(
+											function(config,info) {
+
+												var projectName = config[0]["project.name"];
+												var projectDescription = config[0]["project.description"];
+												var projectLogo = config[0]["project.header.logo"];
+												var projectColor = config[0]["project.header.title.color"];
+												var projectBg = config[0]["project.header.background"];
+
+												if (typeof projectBg !== 'undefined'
+														&& projectBg
+																.substring(
+																		0,
+																		8) == "resteasy") {
+													projectBg = value
+															+ '/'
+															+ projectBg;
+												}
+												if (typeof projectBg !== 'undefined'
+														&& projectBg
+																.substring(
+																		0,
+																		6) == "images") {
+													projectBg = value
+															+ '/'
+															+ projectBg;
+												}
+												if (typeof projectLogo !== 'undefined'
+													&& projectLogo
+															.substring(
+																	0,
+																	8) == "resteasy") {
+												projectLogo = value
+														+ '/'
+														+ projectLogo;
+												}
+												if (typeof projectLogo !== 'undefined'
+														&& projectLogo
+																.substring(
+																		0,
+																		6) == "images") {
+													projectLogo = value
+															+ '/'
+															+ projectLogo;
+												}
+
+												var bytes = info[0]["Total number of bytes"];
+												var datasets_display = seadSpaces
+														.abbreviateNumber(info[0]["Datasets"]);
+												var datasets_raw = info[0]["Datasets"];
+												var users = seadSpaces
+														.abbreviateNumber(info[0]["Number of Users"]);
+												var users_raw = info[0]["Number of Users"];
+												var views = seadSpaces
+														.abbreviateNumber(info[0]["Total Views"]);
+												var views_raw = info[0]["Total Views"];
+												var collections = seadSpaces
+														.abbreviateNumber(info[0]["Collections"]);
+												var collections_raw = info[0]["Collections"];
+												var published = seadSpaces
+														.abbreviateNumber(info[0]["Published Collections"]);
+											
+												var bytes_display = seadSpaces.formatBytes(bytes, 2);
+												var img = 'url('+projectLogo+'),url('+projectBg+')';
+												if(projectName == 'SEAD Demo Space'){var img = 'url('+projectBg+')';}
+												spaceWrapper.find('.fade-in-content').text(projectDescription);
+												if(projectDescription == ''){spaceWrapper.find('.fade-in').remove();}
+												spaceWrapper.find('.name').text(projectName);
+												spaceWrapper.find('.views_raw').text(views_raw);
+												spaceWrapper.find('.views').append(views);
+												spaceWrapper.find('.views').attr('title',views+' views');
+												spaceWrapper.find('.users_raw').text(users_raw);
+												spaceWrapper.find('.teammates').append(users);
+												spaceWrapper.find('.collections_raw').text(collections_raw);
+												spaceWrapper.find('.collections').append(collections);
+												spaceWrapper.find('.datasets_raw').text(datasets_raw);
+												spaceWrapper.find('.datasets').append(datasets_display);
+												spaceWrapper.find('.bytes_raw').text(bytes);
+												spaceWrapper.find('.bytes').append(bytes_display);
+												spaceWrapper.find('.published').text(published);
+												spaceWrapper.find('.fade-out').css('background-image',img);
+												
+												spaceWrapper.find('.loading-spinner').remove();
+												
+											})
+									.fail();
+
+						});
+
+	
+	}
+	else {clearInterval(checkLoading);seadSpaces.initSort();}
+}
+
 seadSpaces.init();
 seadSpaces.demoSpace();
+var checkLoading = setInterval(seadSpaces.loadSlowSpaces, 1000);
