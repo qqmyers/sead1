e6bce8119419842ed0b51253585fb2bd53a84038
Jim Myers <myersjd@umich.edu>
2016-07-22 15:41:11 -0400

    changing to public client crential token request type...

--- edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/OrcidClient.java
+++ edu.illinois.ncsa.mmdb.web/src/edu/illinois/ncsa/mmdb/web/server/OrcidClient.java
@@ -34,8 +34,8 @@ import edu.illinois.ncsa.mmdb.web.common.ConfigurationKey;
  */
 public class OrcidClient {
 
-    private static String ORCID_TOKEN_REQUEST_URL   = "https://api.orcid.org/oauth/token";
-    private static String ORCID_PUBLIC_API_BASE_URL = "http://api.orcid.org/v1.2/";
+    private static String ORCID_TOKEN_REQUEST_URL   = "https://pub.orcid.org/oauth/token";
+    private static String ORCID_PUBLIC_API_BASE_URL = "http://pub.orcid.org/v1.2/";
 
     private static Log    log                       = LogFactory.getLog(OrcidClient.class);
 
@@ -58,11 +58,11 @@ public class OrcidClient {
         StringBuilder sb = new StringBuilder();
         sb.append("client_id=" + clientId + "&");
         sb.append("client_secret=" + clientSecret + "&");
-        sb.append("grant_type=" + "authorization_code&");
+        sb.append("grant_type=" + "client_credentials&");
         if (redirectURI != null) {
             sb.append("redirect_uri=" + redirectURI + "&");
         }
-        sb.append("code=" + code);
+        //sb.append("code=" + code);
 
         authorize.body(MediaType.APPLICATION_FORM_URLENCODED_TYPE, sb.toString());
 
